f5f33ddd59717a11c2aa02e8f262d9213f2d5f2d
liuyunan <liuyunan.20@gmail.com>
2022-02-22 01:03:34 -0800

    add a better solution

--- merge_two_sorted_lists.py
+++ merge_two_sorted_lists.py
@@ -7,7 +7,8 @@ class ListNode:
 
 class Solution:
 
-    def mergeTwoLists(self, list1: ListNode, list2: ListNode) -> ListNode:
+    # solution I thought with many judgement
+    def mergeTwoLists_1(self, list1: ListNode, list2: ListNode) -> ListNode:
         if list1 is None:
             return list2
         elif list2 is None:
@@ -21,7 +22,7 @@ class Solution:
             else:
                 head = list2
                 l3 = list2
-                list2= list2.next
+                list2 = list2.next
 
             while True:
                 if list1 is None:
@@ -39,3 +40,20 @@ class Solution:
                     l3 = l3.next
                     list2 = list2.next
             return head
+    # solution of using a virtual previous node, then return .next of that node
+    def mergeTwoLists_2(self, list1: ListNode, list2: ListNode) -> ListNode:
+        new_head = ListNode()
+        p_node = new_head
+        while list1 and list2:
+            if list1.val <= list2.val:
+                p_node.next = list1
+                list1 = list1.next
+            else:
+                p_node.next = list2
+                list2 = list2.next
+            p_node = p_node.next
+        if list1 is None:
+            p_node.next = list2
+        else:
+            p_node.next = list1
+        return new_head.next
