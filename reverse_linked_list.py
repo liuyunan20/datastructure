142df17fd89a28c7c3370978447fe72f55f803ae
liuyunan <liuyunan.20@gmail.com>
2022-02-22 13:24:44 -0800

    add a better solution

--- reverse_linked_list.py
+++ reverse_linked_list.py
@@ -6,7 +6,8 @@ class ListNode:
 
 
 class Solution:
-    def reverseList(self, head: ListNode) -> ListNode:
+    # return a new liked list reverse to the input one
+    def reverseList_1(self, head: ListNode) -> ListNode:
         if head:
             l1 = ListNode(head.val, None)
             l2 = l1
@@ -16,4 +17,15 @@ class Solution:
                 head = head.next
         else:
             l2 = None
+        return l2
+
+    # reverse the input linked list and return it
+    def reverseList_2(self, head: ListNode) -> ListNode:
+        l2 = None
+        while head:
+            l1 = ListNode(-1, head.next)
+            head.next = l2
+            l2 = head
+            head = l1.next
+
         return l2
