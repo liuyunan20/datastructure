8afb17a205f71efbcfc21e3ecb2b96fef3af5c44
liuyunan <liuyunan.20@gmail.com>
2022-02-24 19:46:53 -0800

    delete else after return

--- maximum_depth_of_binary_tree.py
+++ maximum_depth_of_binary_tree.py
@@ -8,21 +8,21 @@ class TreeNode:
         self.left = left
         self.right = right
 
-        
+
 class Solution:
     def maxDepth(self, root: Optional[TreeNode]) -> int:
         if not root:
             return 0
-        else:
-            current_level_nodes = [root]
-            level = 0
-            while current_level_nodes:
-                next_level_nodes = []
-                for node in current_level_nodes:
-                    if node:
-                        next_level_nodes.append(node.left)
-                        next_level_nodes.append(node.right)
-                if next_level_nodes:
-                    level += 1
-                current_level_nodes = next_level_nodes
-            return level
+        
+        current_level_nodes = [root]
+        level = 0
+        while current_level_nodes:
+            next_level_nodes = []
+            for node in current_level_nodes:
+                if node:
+                    next_level_nodes.append(node.left)
+                    next_level_nodes.append(node.right)
+            if next_level_nodes:
+                level += 1
+            current_level_nodes = next_level_nodes
+        return level
