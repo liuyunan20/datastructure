58df91bb606a470c2196bc6d879ee8ecb3cfc70f
liuyunan <liuyunan.20@gmail.com>
2022-03-03 21:14:50 -0800

    add an iterative solution, pop a node from stack, if it's a leaf append its value to result, if it has right child, push the right child to stack first, then push itself, then left, change this node as a leaf

--- binary_tree_inorder_traversal.py
+++ binary_tree_inorder_traversal.py
@@ -10,10 +10,30 @@ class TreeNode:
 
 
 class Solution:
-    def inorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+    # recursion solution
+    def inorderTraversal_1(self, root: Optional[TreeNode]) -> List[int]:
         inorder_tree = []
         if root:
             inorder_tree += self.inorderTraversal(root.left)
             inorder_tree.append(root.val)
             inorder_tree += self.inorderTraversal(root.right)
+        return inorder_tree
+
+    # stack solution
+    def inorderTraversal_2(self, root: Optional[TreeNode]) -> List[int]:
+        inorder_tree = []
+        my_stack = [root]
+        if root:
+            while my_stack:
+                node = my_stack.pop()
+                if not node.left and not node.right:
+                    inorder_tree.append(node.val)
+                else:
+                    if node.right:
+                        my_stack.append(node.right)
+                    my_stack.append(node)
+                    if node.left:
+                        my_stack.append(node.left)
+                    node.left = None
+                    node.right = None
         return inorder_tree
