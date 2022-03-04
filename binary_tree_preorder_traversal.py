1b6f4f9a04be08b381c7c2694f4e2530d60b1e5f
liuyunan <liuyunan.20@gmail.com>
2022-03-03 19:40:50 -0800

    add an iterative solution, pop a node from stack, if it has right child, push the right child to stack first, then check left

--- binary_tree_preorder_traversal.py
+++ binary_tree_preorder_traversal.py
@@ -10,10 +10,26 @@ class TreeNode:
 
 
 class Solution:
-    def preorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+    # recursion solution
+    def preorderTraversal_1(self, root: Optional[TreeNode]) -> List[int]:
         preorder_tree = []
         if root:
             preorder_tree.append(root.val)
             preorder_tree += self.preorderTraversal(root.left)
             preorder_tree += self.preorderTraversal(root.right)
+        return preorder_tree
+
+    # iterative solution
+    def preorderTraversal_2(self, root: Optional[TreeNode]) -> List[int]:
+        my_stack = [root]
+        preorder_tree = []
+        if root:
+            while my_stack:
+                node = my_stack.pop()
+                preorder_tree.append(node.val)
+                if node.right:
+                    my_stack.append(node.right)
+                if node.left:
+                    my_stack.append(node.left)
+
         return preorder_tree
