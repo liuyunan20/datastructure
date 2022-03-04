1ccf047cd3daeb140df110eafa75f15a198ae6e7
liuyunan <liuyunan.20@gmail.com>
2022-03-03 21:22:58 -0800

    add an iterative solution, pop a node from stack, if it's a leaf append its value to result, then push itself to stack, if it has right child, push the right child to stack, then left, change this node as a leaf

--- binary_tree_postorder_traversal.py
+++ binary_tree_postorder_traversal.py
@@ -10,10 +10,31 @@ class TreeNode:
 
 
 class Solution:
-    def postorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+    # recursion solution
+    def postorderTraversal_1(self, root: Optional[TreeNode]) -> List[int]:
         postorder_tree = []
         if root:
-            postorder_tree += self.postorderTraversal(root.left)
-            postorder_tree += self.postorderTraversal(root.right)
+            postorder_tree += self.postorderTraversal_1(root.left)
+            postorder_tree += self.postorderTraversal_1(root.right)
             postorder_tree.append(root.val)
+        return postorder_tree
+
+    # stack solution
+    def postorderTraversal_2(self, root: Optional[TreeNode]) -> List[int]:
+        postorder_tree = []
+        my_stack = [root]
+        if root:
+            while my_stack:
+                node = my_stack.pop()
+                if not node.left and not node.right:
+                    postorder_tree.append(node.val)
+                else:
+                    my_stack.append(node)
+                    if node.right:
+                        my_stack.append(node.right)
+                    if node.left:
+                        my_stack.append(node.left)
+                    node.left = None
+                    node.right = None
+
         return postorder_tree
