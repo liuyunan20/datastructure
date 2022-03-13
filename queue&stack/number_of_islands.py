616f23c318f195d93f8369ec02c692d5d9e2b47a
liuyunan <liuyunan.20@gmail.com>
2022-03-12 23:20:16 -0800

    also exceed time limit

--- queue&stack/number_of_islands.py
+++ queue&stack/number_of_islands.py
@@ -3,7 +3,7 @@ from typing import List
 
 class Solution:
     # TLE version
-    def numIslands(self, grid: List[List[str]]) -> int:
+    def numIslands_TLE1(self, grid: List[List[str]]) -> int:
         m = len(grid)
         n = len(grid[0])
         visited = []
@@ -31,4 +31,37 @@ class Solution:
                                 visited.append((x, y + 1))
                                 island_edge.append((x, y + 1))
                         current_island = island_edge
+        return result
+
+    def numIslands_TLE2(self, grid: List[List[str]]) -> int:
+        m = len(grid)
+        n = len(grid[0])
+        lands = []
+        result = 0
+        for i in range(m):
+            for j in range(n):
+                if grid[i][j] == '1':
+                    lands.append((i, j))
+        while lands:
+            i, j = lands.pop(0)
+            current_island = []
+            result += 1
+            current_island.append((i, j))
+            while current_island:
+                island_edge = []
+                for x, y in current_island:
+                    if x - 1 >= 0 and grid[x - 1][y] == '1' and (x - 1, y) in lands:
+                        island_edge.append((x - 1, y))
+                        lands.remove((x - 1, y))
+                    if x + 1 < m and grid[x + 1][y] == '1' and (x + 1, y) in lands:
+                        lands.remove((x + 1, y))
+                        island_edge.append((x + 1, y))
+                    if y - 1 >= 0 and grid[x][y - 1] == '1' and (x, y - 1) in lands:
+                        island_edge.append((x, y - 1))
+                        lands.remove((x, y - 1))
+                    if y + 1 < n and grid[x][y + 1] == '1' and (x, y + 1) in lands:
+                        lands.remove((x, y + 1))
+                        island_edge.append((x, y + 1))
+                current_island = island_edge
+
         return result
