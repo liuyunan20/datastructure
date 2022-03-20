b9ce9007bcc2504449a34017b21f061cb5eeee7c
liuyunan <liuyunan.20@gmail.com>
2022-03-20 01:12:02 -0700

    You are given an integer array nums and an integer target.
    You want to build an expression out of nums by adding one of the symbols '+' and '-' before each integer in nums and then concatenate all the integers.
    
    For example, if nums = [2, 1], you can add a '+' before 2 and a '-' before 1 and concatenate them to build the expression "+2-1".
    Return the number of different expressions that you can build, which evaluates to target.

---
+++ queue&stack/target_sum.py
@@ -0,0 +1,20 @@
+from typing import List
+
+
+class Solution:
+    # TLE, maybe because of recursion
+    def findTargetSumWays(self, nums: List[int], target: int) -> int:
+        def dfs(nums, target, index):
+            if index == len(nums):
+                return 0
+            if index == len(nums) - 1:
+                if nums[index] == abs(target):
+                    if nums[index] == 0:
+                        return 2
+                    return 1
+                return 0
+            num = nums[index]
+            a = dfs(nums, target + num, index + 1)
+            b = dfs(nums, target - num, index + 1)
+            return a + b
+        return dfs(nums, target, 0)
