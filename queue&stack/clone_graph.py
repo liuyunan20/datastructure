f3cbadc434073d28431e6645e9a47232e9aa5727
liuyunan <liuyunan.20@gmail.com>
2022-03-19 20:32:25 -0700

    Given a reference of a node in a connected undirected graph.
    Return a deep copy (clone) of the graph.
    
    Each node in the graph contains a value (int) and a list (List[Node]) of its neighbors.
    
    class Node {
        public int val;
        public List<Node> neighbors;
    }

---
+++ queue&stack/clone_graph.py
@@ -0,0 +1,27 @@
+class Node:
+    def __init__(self, val = 0, neighbors = None):
+        self.val = val
+        self.neighbors = neighbors if neighbors is not None else []
+        
+
+class Solution:
+    def cloneGraph(self, node: 'Node') -> 'Node':
+        def dfs(node, visited):
+            if not node:
+                return None
+            if node not in visited:
+                node_clone = Node(node.val)
+                visited[node.val] = node_clone
+            if not node.neighbors:
+                node_clone.neighbors = []
+            else:
+                for nei in node.neighbors:
+                    if nei.val in visited:
+                        node_clone.neighbors.append(visited[nei.val])
+                    else:
+                        node_clone.neighbors.append(dfs(nei, visited))
+            return node_clone
+
+        visited = {}
+        root = dfs(node, visited)
+        return root
