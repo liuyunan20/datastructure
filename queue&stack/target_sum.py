c30ab84a2d5cdd750d0e0de10d139542d6341821
liuyunan <liuyunan.20@gmail.com>
2022-03-20 21:08:53 -0700

    use cache tool to cover tle

--- queue&stack/target_sum.py
+++ queue&stack/target_sum.py
@@ -1,9 +1,10 @@
+from functools import cache
 from typing import List
 
 
 class Solution:
     # TLE, maybe because of recursion
-    def findTargetSumWays(self, nums: List[int], target: int) -> int:
+    def findTargetSumWays_tle(self, nums: List[int], target: int) -> int:
         def dfs(nums, target, index):
             if index == len(nums):
                 return 0
@@ -18,3 +19,20 @@ class Solution:
             b = dfs(nums, target - num, index + 1)
             return a + b
         return dfs(nums, target, 0)
+
+    def findTargetSumWays(self, nums: List[int], target: int) -> int:
+        @cache
+        def dfs(target, index):
+            if index == len(nums):
+                return 0
+            if index == len(nums) - 1:
+                if nums[index] == abs(target):
+                    if nums[index] == 0:
+                        return 2
+                    return 1
+                return 0
+            num = nums[index]
+            a = dfs(target + num, index + 1)
+            b = dfs(target - num, index + 1)
+            return a + b
+        return dfs(target, 0)
