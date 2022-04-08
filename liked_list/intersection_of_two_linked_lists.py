c0c841cc7cf25a624028208cb65e5d59639eb65c
liuyunan <liuyunan.20@gmail.com>
2022-04-08 01:07:24 -0700

    create two lists of all nodes value, find out the longest common sublist, then get both indexes of the start node with same value. From this position, judge if every two nodes from two lists are same node, if yes, this is where the intersection starts.

--- liked_list/intersection_of_two_linked_lists.py
+++ liked_list/intersection_of_two_linked_lists.py
@@ -19,3 +19,36 @@ class Solution:
             a = a.next
             b = headB
         return None
+    # solution using list of all node value
+    def getIntersectionNode_2(self, headA: ListNode, headB: ListNode) -> Optional[ListNode]:
+        a, b = headA, headB
+        a_list = []
+        b_list = []
+        while a:
+            a_list.append(a.val)
+            a = a.next
+        while b:
+            b_list.append(b.val)
+            b = b.next
+        a_list = a_list[::-1]
+        b_list = b_list[::-1]
+        i = 0
+        for a, b in zip(a_list, b_list):
+            if a != b:
+                break
+            i += 1
+        if i == 0:
+            return None
+        a_index = len(a_list) - i
+        b_index = len(b_list) - i
+        a, b = headA, headB
+        while a_index:
+            a = a.next
+            a_index -= 1
+        while b_index:
+            b = b.next
+            b_index -= 1
+        while a != b:
+            a = a.next
+            b = b.next
+        return a
