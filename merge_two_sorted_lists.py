43bbaa0d40a5f28b4e69155c7d237190f487fc1d
liuyunan <liuyunan.20@gmail.com>
2022-04-12 13:43:12 -0700

    review old problem

--- merge_two_sorted_lists.py
+++ merge_two_sorted_lists.py
@@ -1,4 +1,7 @@
 # Definition for singly-linked list.
+from typing import Optional
+
+
 class ListNode:
     def __init__(self, val=0, next=None):
         self.val = val
@@ -57,3 +60,30 @@ class Solution:
         else:
             p_node.next = list1
         return new_head.next
+
+
+    def mergeTwoLists_3(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
+        if not list1:
+            return list2
+        if not list2:
+            return list1
+        if list1.val <= list2.val:
+            head = list1
+            list1 = list1.next
+        else:
+            head = list2
+            list2 = list2.next
+        node = head
+        while list1 and list2:
+            if list1.val <= list2.val:
+                node.next = list1
+                list1 = list1.next
+            else:
+                node.next = list2
+                list2 = list2.next
+            node = node.next
+        if list1:
+            node.next = list1
+        if list2:
+            node.next = list2
+        return head
