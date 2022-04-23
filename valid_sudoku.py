84c6165bbafcab80456623121d352227c3092f26
liuyunan <liuyunan.20@gmail.com>
2022-04-23 00:19:08 -0700

    use hashmap to check if a sodoku board is valid

--- valid_sudoku.py
+++ valid_sudoku.py
@@ -24,3 +24,29 @@ class Solution:
                     else:
                         return False
         return True
+
+    def isValidSudoku_hashmap(self, board: List[List[str]]) -> bool:
+        digit_position = {}
+        m = len(board)
+        n = len(board[0])
+        for i in range(m):
+            for j in range(n):
+                if board[i][j] != ".":
+                    digit_position.setdefault(board[i][j], []).append((i, j))
+        for digit in digit_position:
+            j_set = set()
+            i_set = set()
+            for i, j in digit_position[digit]:
+                i_set.add(i)
+                j_set.add(j)
+            if len(j_set) < len(digit_position[digit]) or len(i_set) < len(digit_position[digit]):
+                return False
+        block ={}
+        for i in range(9):
+            for x in range(3):
+                for y in range(3):
+                    block.setdefault(i, []).append((3 * (i // 3) + x, 3 * (i % 3) + y))
+            for digit in digit_position:
+                if len(set(digit_position[digit])&set(block[i])) > 1:
+                    return False
+        return True
