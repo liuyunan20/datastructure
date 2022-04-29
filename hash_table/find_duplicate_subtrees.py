cedbf72eebfeb705f5fae5bbf1091384611ad6bd
liuyunan <liuyunan.20@gmail.com>
2022-04-29 11:48:59 -0700

    Given the root of a binary tree, return all duplicate subtrees.
    For each kind of duplicate subtrees, you only need to return the root node of any one of them.
    
    Two trees are duplicate if they have the same structure with the same node values.

---
+++ hash_table/find_duplicate_subtrees.py
@@ -0,0 +1,31 @@
+# Definition for a binary tree node.
+from typing import Optional, List
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+        
+class Solution:
+    def findDuplicateSubtrees(self, root: Optional[TreeNode]) -> List[Optional[TreeNode]]:
+        subtrees = {}
+        def traversal(node):
+            tree = ''
+            if node is None:
+                return 'None'
+            tree += str(node.val) + ' '
+            tree += traversal(node.left) + ' '
+            tree += traversal(node.right) + ' '
+            subtrees.setdefault(tree, [])
+            subtrees[tree].append(node)
+            return tree
+
+        traversal(root)
+        result = []
+        for subtree in subtrees:
+            if len(subtrees[subtree]) >= 2:
+                result.append(subtrees[subtree][0])
+        return result
