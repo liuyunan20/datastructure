b0bda23d4ab0a2654a3525cd39a3974b9d050727
liuyunan <liuyunan.20@gmail.com>
2022-05-22 23:23:58 -0700

    add recursion solution

--- binary_search_tree/insert_into_a_binary_search_tree.py
+++ binary_search_tree/insert_into_a_binary_search_tree.py
@@ -29,3 +29,19 @@ class Solution:
                 if node.val < val:
                     next_level_nodes.append(node.right)
             current_level_nodes = next_level_nodes
+
+    def insertIntoBST_recursion(self, root: Optional[TreeNode], val: int) -> Optional[TreeNode]:
+        if root is None:
+            return TreeNode(val)
+
+        if val > root.val:
+            if root.right:
+                self.insertIntoBST(root.right, val)
+            else:
+                root.right = TreeNode(val)
+        if val < root.val:
+            if root.left:
+                self.insertIntoBST(root.left, val)
+            else:
+                root.left = TreeNode(val)
+        return root
