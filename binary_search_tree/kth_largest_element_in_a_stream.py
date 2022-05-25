6d77c87371ac7e0218bfdbd3c2b61e6404186c49
liuyunan <liuyunan.20@gmail.com>
2022-05-24 23:17:37 -0700

    Design a class to find the kth largest element in a stream. Note that it is the kth largest element in the sorted order, not the kth distinct element.
    Implement KthLargest class:
    
    KthLargest(int k, int[] nums) Initializes the object with the integer k and the stream of integers nums.
    int add(int val) Appends the integer val to the stream and returns the element representing the kth largest element in the stream.

---
+++ binary_search_tree/kth_largest_element_in_a_stream.py
@@ -0,0 +1,14 @@
+class KthLargest_tle:
+
+    def __init__(self, k: int, nums: List[int]):
+        self.nums = nums
+        self.k = k
+
+    def add(self, val: int) -> int:
+        self.nums.append(val)
+        return sorted(self.nums, reverse=True)[self.k - 1]
+
+
+# Your KthLargest object will be instantiated and called as such:
+# obj = KthLargest(k, nums)
+# param_1 = obj.add(val)
