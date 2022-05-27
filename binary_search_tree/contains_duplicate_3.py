ae1bd163867062843e735cd8775cf48f129a2411
liuyunan <liuyunan.20@gmail.com>
2022-05-27 16:01:38 -0700

    Given an integer array nums and two integers k and t, return true if there are two distinct indices i and j in the array such that abs(nums[i] - nums[j]) <= t and abs(i - j) <= k.

---
+++ binary_search_tree/contains_duplicate_3.py
@@ -0,0 +1,10 @@
+from typing import List
+
+
+class Solution:
+    def containsNearbyAlmostDuplicate(self, nums: List[int], k: int, t: int) -> bool:
+        for i, num in enumerate(nums):
+            for j in range(i + 1, min(i + k + 1, len(nums))):
+                if abs(num - nums[j]) <= t:
+                    return True
+        return False
