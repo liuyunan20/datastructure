c3d103c87bc4e19915ad59a8bafd7023a39cffd0
liuyunan <liuyunan.20@gmail.com>
2022-05-28 01:14:53 -0700

    use sorted list to contain k numbers, add a new number and remove the oldest number to keep the length of the sorted list is k, compare the new number with its neighbors, if the absolute difference is less or equal to t, return True.

--- binary_search_tree/contains_duplicate_3.py
+++ binary_search_tree/contains_duplicate_3.py
@@ -1,4 +1,5 @@
 from typing import List
+from sortedcontainers import SortedList
 
 
 class Solution:
@@ -7,4 +8,16 @@ class Solution:
             for j in range(i + 1, min(i + k + 1, len(nums))):
                 if abs(num - nums[j]) <= t:
                     return True
+        return False
+
+# use sorted list to contain k numbers
+def containsNearbyAlmostDuplicate(self, nums: List[int], k: int, t: int) -> bool:
+        sl = SortedList()
+        for i, num in enumerate(nums):
+            sl.add(num)
+            if i > k:
+                sl.remove(nums[i - k - 1])
+            index = sl.index(num)
+            if (index > 0 and abs(sl[index - 1] - num) <= t) or (index < len(sl) - 1 and abs(sl[index + 1] - num) <= t):
+                return True
         return False
