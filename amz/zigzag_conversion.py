891e95500a60f396b087b6ccdc1d0fe5c63572e2
liuyunan <liuyunan.20@gmail.com>
2022-06-13 14:25:43 -0700

    calculate every row consist of letters on which position of the string. Visit all characters in row 0 first, then row 1, then row 2, and so on...
    For all whole numbers k,
    
    Characters in row 00 are located at indexes k(2⋅numRows−2)
    Characters in row numRows−1 are located at indexes k(2⋅numRows−2)+numRows−1
    Characters in inner row ii are located at indexes k(2⋅numRows−2)+i and (k+1)(2⋅numRows−2)−i.

--- amz/zigzag_conversion.py
+++ amz/zigzag_conversion.py
@@ -32,3 +32,18 @@ class Solution:
         for i in range(len(zz)):
             zz[i] = ''.join(zz[i])
         return ''.join(zz)
+
+    def convert_visit_row(self, s: str, numRows: int) -> str:
+        if numRows == 1:
+            return s
+        s = list(s)
+        zz = ['' for _ in range(numRows)]
+        col = math.ceil(len(s) / (2 * numRows - 2)) + 1
+        for i in range(numRows):
+            for j in range(col):
+                if i != 0 and i != numRows - 1:
+                    if 0 < j*(2*numRows-2)-i < len(s):
+                        zz[i] += s[j*(2*numRows-2)-i]
+                if j*(2*numRows-2)+i < len(s):
+                    zz[i] += s[j*(2*numRows-2)+i]
+        return ''.join(zz)
