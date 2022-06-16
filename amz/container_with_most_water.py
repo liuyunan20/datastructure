6ec5c07e45ef6abbd937b64f95bd92c65fcd0840
liuyunan <liuyunan.20@gmail.com>
2022-06-15 23:36:17 -0700

    from head and tail to middle indexes, calculate water between them, move the lower one to middle one step.

--- amz/container_with_most_water.py
+++ amz/container_with_most_water.py
@@ -10,3 +10,16 @@ class Solution:
                 water = max(water, min(height[j], h)*(i-j))
             result = max(water, result)
         return result
+
+
+    def maxArea(self, height: List[int]) -> int:
+        result = 0
+        i = 0
+        j = len(height) - 1
+        while i < j:
+            result = max(result, min(height[i], height[j])*(j-i))
+            if height[i] <= height[j]:
+                i += 1
+            else:
+                j -= 1
+        return result
