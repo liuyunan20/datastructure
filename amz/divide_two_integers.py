c9aa94fd17c7ec29d52764200f1293c500a23e4d
liuyunan <liuyunan.20@gmail.com>
2022-06-19 00:20:25 -0700

    Given two integers dividend and divisor, divide two integers without using multiplication, division, and mod operator.
    The integer division should truncate toward zero, which means losing its fractional part. For example, 8.345 would be truncated to 8, and -2.7335 would be truncated to -2.
    
    Return the quotient after dividing dividend by divisor.
    
    Note: Assume we are dealing with an environment that could only store integers within the 32-bit signed integer range: [−231, 231 − 1]. For this problem, if the quotient is strictly greater than 231 - 1, then return 231 - 1, and if the quotient is strictly less than -231, then return -231.

---
+++ amz/divide_two_integers.py
@@ -0,0 +1,22 @@
+class Solution:
+    def divide_tle(self, dividend: int, divisor: int) -> int:
+        positive = True
+        if (dividend > 0 and divisor < 0) or (dividend < 0 and divisor > 0):
+            positive = False
+
+        dividend = abs(dividend)
+        divisor = abs(divisor)
+
+        counter = 0
+        while  dividend >= divisor:
+            if divisor == 1:
+                counter = dividend
+                break
+            dividend -= divisor
+            counter += 1
+        if positive:
+            if counter >= 2147483648:
+                return 2147483647
+            return counter
+        else:
+            return -counter
