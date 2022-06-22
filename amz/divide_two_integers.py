4a6f4325734f15f5d9221d69d526e789018e4976
liuyunan <liuyunan.20@gmail.com>
2022-06-21 23:33:50 -0700

    double the divisor every time to reach the dividend quickly

--- amz/divide_two_integers.py
+++ amz/divide_two_integers.py
@@ -1,24 +1,28 @@
 class Solution:
-    def divide_tle(self, dividend: int, divisor: int) -> int:
-        max_int = 2147483647
-        min_int = -MAX_INT = 2147483648
+    def divide(self, dividend: int, divisor: int) -> int:
         positive = True
         if (dividend > 0 and divisor < 0) or (dividend < 0 and divisor > 0):
             positive = False
-
+        max_int = 2147483647
         dividend = abs(dividend)
         divisor = abs(divisor)
-
-        counter = 0
-        while  dividend >= divisor:
-            if divisor == 1:
-                counter = dividend
-                break
-            dividend -= divisor
-            counter += 1
+        if divisor == 1:
+            counter = dividend
+        result = 0
+        while dividend >= divisor:
+            counter = 0
+            divisor_copy = divisor
+            while dividend >= divisor_copy:
+                root = divisor_copy
+                divisor_copy += divisor_copy
+                counter += 1
+            divisor_copy -= root
+            counter -= 1
+            result += 2 ** counter
+            dividend -= divisor_copy
         if positive:
-            if counter >= 2147483648:
-                return 2147483647
-            return counter
+            if result > max_int:
+                return max_int
+            return result
         else:
-            return -counter
+            return -result
