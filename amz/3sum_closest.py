b7ad2f821d24e27b2ac3178fef8da34ee22d6d55
liuyunan <liuyunan.20@gmail.com>
2022-06-28 20:32:12 -0700

    something wrong

--- amz/3sum_closest.py
+++ amz/3sum_closest.py
@@ -1,4 +1,5 @@
 from typing import List
+from sortedcontainers import SortedList
 
 
 class Solution:
@@ -18,3 +19,25 @@ class Solution:
                 else:
                     m -= 1
         return target - dif
+
+    def threeSumClosest_wrong(self, nums: List[int], target: int) -> int:
+        num_dict = {}
+        dif = 2147483647
+        for num in nums:
+            num_dict.setdefault(num, 0)
+            num_dict[num] += 1
+        nums = SortedList(num_dict.keys())
+        for i in range(len(nums)-1):
+            for j in range(i+1, len(nums)):
+                num = target-nums[i]-nums[j]
+                if not num_dict.get(num):
+                    index = nums.bisect_left(num)
+                    if abs(dif) > abs(nums[index] - num):
+                        dif = nums[index] - num
+                    index = nums.bisect_right(num)
+                    if abs(dif) > abs(nums[index] - num):
+                        dif = nums[index] - num
+                else:
+                    if (num != nums[i] and num != nums[j]) or ((num == nums[i] and num_dict[nums[i]] > 1) or (num == nums[j] and num_dict[nums[j]] > 1)):
+                        return target
+        return target + dif
