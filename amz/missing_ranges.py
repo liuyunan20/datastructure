ef0aecc7e812b5ac3572ba28a581b5c1fc38bc6e
liuyunan <liuyunan.20@gmail.com>
2022-07-03 23:41:58 -0700

    still slow

--- amz/missing_ranges.py
+++ amz/missing_ranges.py
@@ -19,3 +19,24 @@ class Solution:
             else:
                 result[i] = str(el[0]) + '->' + str(el[1])
         return result
+
+    def findMissingRanges(self, nums: List[int], lower: int, upper: int) -> List[str]:
+        result = []
+        if not nums:
+            if lower != upper:
+                return [str(lower) + '->' + str(upper)]
+            return [str(lower)]
+        if lower != nums[0]:
+            result.append([lower, nums[0]-1])
+        for i in range(len(nums)-1):
+            if nums[i] < nums[i+1] - 1:
+                result.append([nums[i]+1, nums[i+1]-1])
+        if upper != nums[-1]:
+            result.append([nums[-1]+1, upper])
+
+        for i, el in enumerate(result):
+            if el[0] == el[1]:
+                result[i] = str(el[0])
+            else:
+                result[i] = str(el[0]) + '->' + str(el[1])
+        return result
