9b0e46ac9ceb9a23315362ff92b9b7cae5bc3ef5
liuyunan <liuyunan.20@gmail.com>
2022-07-05 21:35:31 -0700

    sort nums, map all numbers and their appearance times, calculate 3 nums, find the 4th number in the map.

--- amz/4sum.py
+++ amz/4sum.py
@@ -23,3 +23,28 @@ class Solution:
                         if i != m and i != n and j != m and j != n:
                             result.add(tuple(sorted([nums[i], nums[j], nums[m], nums[n]])))
         return result
+
+    def fourSum(self, nums: List[int], target: int) -> List[List[int]]:
+        result = []
+        num_dict = {}
+        for num in nums:
+            num_dict.setdefault(num, 0)
+            num_dict[num] += 1
+        nums.sort()
+        for i in range(len(nums)-2):
+            if i > 0 and nums[i] == nums[i-1]:
+                continue
+            for j in range(i+1, len(nums)-1):
+                if j > i+1 and nums[j] == nums[j-1]:
+                    continue
+                for k in range(j+1, len(nums)):
+                    if k > j+1 and nums[k] == nums[k-1]:
+                        continue
+                    n = target - nums[i] - nums[j] - nums[k]
+                    if n > nums[k] and num_dict.get(n) is not None:
+                        result.append([nums[i], nums[j], nums[k], n])
+                    if n == nums[k]:
+                        s = int(n == nums[i]) + int(n == nums[j]) + int(n == nums[k])
+                        if num_dict[n] > s:
+                            result.append([nums[i], nums[j], nums[k], n])
+        return result
