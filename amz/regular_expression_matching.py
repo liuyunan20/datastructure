fc747b021ed48c47a6aa5a0319f20817aba8edf1
liuyunan <liuyunan.20@gmail.com>
2022-07-13 10:44:09 -0700

    input: "cbaacacaaccbaabcb" "c*b*b*.*ac*.*bc*a*" can't pass

--- amz/regular_expression_matching.py
+++ amz/regular_expression_matching.py
@@ -21,7 +21,10 @@ class Solution:
                 if p[j] == s[i]:
                     pre = s[i]
                     j += 1
-                    i += 1
+                    if j < len(p) and p[j] == "*" and match(s[i:], p[j+1:], pre):
+                        return True
+                    else:
+                        i += 1
                 elif p[j] == '.':
                     pre = '.'
                     j += 1
@@ -35,34 +38,31 @@ class Solution:
                     else:
                         return False
                 else:
-                    if match(s[i:], p[j+1:], pre):
-                        return True
-                    else:
-                        while pre != '.' and i < len(s) - (len(p) - j - 1) and s[i] == pre:
-                            i += 1
-                        if pre == '.':
-                            while j + 2 < len(p) and p[j+2] == '*':
-                                j += 2
-                            if j + 1 < len(p) and '*' in p[j+1:]:
-                                p2 = p[j+1:]
-                                x = p2.index('*') - 1
-                                p1 = p2[:x]
-                                inds = find_sub_list(p1,s[i:])
-                                if not inds:
-                                    return False
-                                else:
-                                    for ind in inds:
-                                        return match(s[ind+i:], p[j+1:], pre)
+                    while pre != '.' and i < len(s) - (len(p) - j - 1) and s[i] == pre:
+                        i += 1
+                    if pre == '.':
+                        while j + 2 < len(p) and p[j+2] == '*':
+                            j += 2
+                        if j + 1 < len(p) and '*' in p[j+1:]:
+                            p2 = p[j+1:]
+                            x = p2.index('*') - 1
+                            p1 = p2[:x]
+                            inds = find_sub_list(p1,s[i:])
+                            if not inds:
+                                return False
                             else:
-                                i = len(s) - (len(p) - j - 1)
-                                j += 1
-                                continue
-                        if i == len(s) and j == len(p) - 1:
-                            return True
-                        elif i == len(s) and j != len(p) - 1:
-                            return False
+                                for ind in inds:
+                                    return match(s[ind+i:], p[j+1:], pre)
                         else:
+                            i = len(s) - (len(p) - j - 1)
                             j += 1
+                            continue
+                    if i == len(s) and j == len(p) - 1:
+                        return True
+                    elif i == len(s) and j != len(p) - 1:
+                        return False
+                    else:
+                        j += 1
             if j == len(p) or (j == len(p) - 1 and p[j] == '*') or (j == len(p) - 2 and p[j+1] == '*'):
                 return True
             return False
