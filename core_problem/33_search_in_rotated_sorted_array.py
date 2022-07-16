602ccd173113c6e1aca07b02bd4368cab6865465
liuyunan <liuyunan.20@gmail.com>
2022-07-16 09:43:59 -0700

    one through binary search

--- core_problem/33_search_in_rotated_sorted_array.py
+++ core_problem/33_search_in_rotated_sorted_array.py
@@ -3,29 +3,19 @@ from typing import List
 
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
-        l, r, offset = 0, len(nums) - 1, 0
-        if nums[l] > nums[r]:
-            while l <= r:
-                m = (l + r) // 2
-                if nums[l] == nums[m]:
-                    if target >= nums[0]:
-                        offset = l - len(nums) + 1
-                    else:
-                        offset = l + 1
-                    nums = nums[l+1:] + nums[:l+1]
-                    break
-                elif nums[l] < nums[m]:
-                    l = m
+        left, right = 0, len(nums) - 1
+        while left <= right:
+            middle = (left + right) // 2
+            if nums[middle] == target:
+                return middle
+            if nums[left] <= nums[middle]:
+                if nums[left] <= target < nums[middle]:
+                    right = middle - 1
                 else:
-                    r = m
-
-        l, r = 0, len(nums) - 1
-        while l <= r:
-            m = (l + r) // 2
-            if nums[m] == target:
-                return m + offset
-            if target > nums[m]:
-                l = m + 1
+                    left = middle + 1
             else:
-                r = m - 1
-        return - 1
+                if nums[middle] < target <= nums[right]:
+                    left = middle + 1
+                else:
+                    right = middle - 1
+        return -1
