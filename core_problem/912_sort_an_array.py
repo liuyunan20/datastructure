a5f2885bd5f7b86efe3344d52e458e9ef3116a38
liuyunan <liuyunan.20@gmail.com>
2022-07-18 23:22:04 -0700

    quick sort, if nums is empty, return it select a random value in the list, go through the list put the same value to middle part, bigger value to right part, smaller value to left part call the function itself with left and right part as argument return left + mid + right

--- core_problem/912_sort_an_array.py
+++ core_problem/912_sort_an_array.py
@@ -17,3 +17,24 @@ class Solution:
                         result.insert(i+1, num)
                         break
         return result
+
+    # quick sort, if nums is empty, return it
+    # select a random value in the list, go through the list
+    # put the same value to middle part, bigger value to right part, smaller value to left part
+    # call the function itself with left and right part as argument
+    # return left + mid + right
+    def sortArray(self, nums: List[int]) -> List[int]:
+        left = []
+        mid = []
+        right = []
+        if not nums:
+            return nums
+        key_value = nums[len(nums)//2]
+        for num in nums:
+            if num == key_value:
+                mid.append(num)
+            if num > key_value:
+                right.append(num)
+            if num < key_value:
+                left.append(num)
+        return self.sortArray(left) + mid + self.sortArray(right)
