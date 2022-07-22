2942484fe0a5747432694550dd74ebcf21a624fe
liuyunan <liuyunan.20@gmail.com>
2022-07-21 21:07:54 -0700

    at each start time, the number of airplanes add 1; at each end time, the number of airplanes minus 1; put all start time and end time with adding or minusing 1 to a list; scan all time, calculate plane numbers return the maximum number

--- core_problem/lintcode391_airplanes.py
+++ core_problem/lintcode391_airplanes.py
@@ -18,7 +18,7 @@ class Solution:
     @param airplanes: An interval array
     @return: Count of airplanes are in the sky.
     """
-    def count_of_airplanes(self, airplanes: List[Interval]) -> int:
+    def count_of_airplanes_tle(self, airplanes: List[Interval]) -> int:
         # write your code here
         if not airplanes:
             return 0
@@ -28,3 +28,19 @@ class Solution:
                 time_plane.setdefault(hour, 0)
                 time_plane[hour] += 1
         return max(time_plane.values())
+
+    def count_of_airplanes(self, airplanes: List[Interval]) -> int:
+        # scan lines
+        if not airplanes:
+            return 0
+        time_plane = []
+        for i in airplanes:
+            time_plane.append((i.start, 1))
+            time_plane.append((i.end, -1))
+        time_plane.sort()
+        plane = 0
+        result = 0
+        for time, p in time_plane:
+            plane += p
+            result = max(plane, result)
+        return result
