a274ddf4f6d125467c65359eb3335df61233ebb1
liuyunan <liuyunan.20@gmail.com>
2022-07-23 23:57:23 -0700

    optimized redundancy code

--- core_problem/53_maximum_subarray.py
+++ core_problem/53_maximum_subarray.py
@@ -15,3 +15,15 @@ class Solution:
                 current += num
             result = max(result, current)
         return result
+
+
+    def maxSubArray_2(self, nums: List[int]) -> int:
+        result = nums[0]
+        current = 0
+        for num in nums:
+            if current < 0:
+                current = num
+            else:
+                current += num
+            result = max(result, current)
+        return result
