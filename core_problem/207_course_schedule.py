2ff12fe0a5af719126fff4b4d893f62d9fc88656
liuyunan <liuyunan.20@gmail.com>
2022-07-27 23:37:04 -0700

    There are a total of numCourses courses you have to take, labeled from 0 to numCourses - 1. You are given an array prerequisites where prerequisites[i] = [ai, bi] indicates that you must take course bi first if you want to take course ai.
    For example, the pair [0, 1], indicates that to take course 0 you have to first take course 1.
    Return true if you can finish all courses. Otherwise, return false.
    
    Example 1:
    
    Input: numCourses = 2, prerequisites = [[1,0]]
    Output: true
    Explanation: There are a total of 2 courses to take.
    To take course 1 you should have finished course 0. So it is possible.
    Example 2:
    
    Input: numCourses = 2, prerequisites = [[1,0],[0,1]]
    Output: false
    Explanation: There are a total of 2 courses to take.
    To take course 1 you should have finished course 0, and to take course 0 you should also have finished course 1. So it is impossible.
    
    Constraints:
    
    1 <= numCourses <= 2000
    0 <= prerequisites.length <= 5000
    prerequisites[i].length == 2
    0 <= ai, bi < numCourses
    All the pairs prerequisites[i] are unique.

---
+++ core_problem/207_course_schedule.py
@@ -0,0 +1,43 @@
+from typing import List
+
+
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        # BFS solution
+        course_step = {}  # store how many preceding courses needed for every course
+        bi_ai = {}  # store course and its following courses
+        queue = []
+        course0 = set()  # store courses without preceding courses
+        # if there is no limit for courses, return True
+        if not prerequisites:
+            return True
+        # initialize course_step and bi_ai
+        for ai, bi in prerequisites:
+            if ai == bi:
+                return False
+            course_step.setdefault(ai, 0)
+            course_step[ai] += 1
+            bi_ai.setdefault(bi, []).append(ai)
+        # find out initial courses without preceding courses
+        for ai, bi in prerequisites:
+            if bi not in course_step:
+                course0.add(bi)
+        # if there's no initial courses, return False
+        if not course0:
+            return False
+        # start BFS searching, find out all courses with 1 preceding course, then 2, 3...
+        # until no following courses could be found
+        for course in course0:
+            queue.append(course)
+            while queue:
+                bi = queue.pop(0)
+                if bi_ai.get(bi):
+                    for ai in bi_ai[bi]:
+                        course_step[ai] -= 1
+                        if course_step[ai] == 0:
+                            queue.append(ai)
+        # check whether there is course which still needs preceding courses, yes return False
+        for course in course_step:
+            if course_step[course] > 0:
+                return False
+        return True
