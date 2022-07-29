8b702276a075d801f54c5116045fac6b0a2a8dca
liuyunan <liuyunan.20@gmail.com>
2022-07-29 15:26:25 -0700

    DFS solution, using recursion, but time limit exceed

--- core_problem/207_course_schedule.py
+++ core_problem/207_course_schedule.py
@@ -41,3 +41,24 @@ class Solution:
             if course_step[course] > 0:
                 return False
         return True
+
+# DFS solution, using recursion, but time limit exceed
+    def canFinish_tle(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        # DFS
+        ai_bi = {}  # store course: [preceding courses]
+        for ai, bi in prerequisites:
+            if ai == bi:
+                return False
+            ai_bi.setdefault(ai, []).append(bi)
+
+        def dfs(course, pres):
+            if not ai_bi.get(course):
+                return True
+            for bi in ai_bi[course]:
+                if bi in pres or not dfs(bi, pres + [bi]):
+                    return False
+            return True
+        for ai in ai_bi:
+            if not dfs(ai, [ai]):
+                return False
+        return True
