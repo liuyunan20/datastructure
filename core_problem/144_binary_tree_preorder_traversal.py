b702205a784f626da07ccf83153841158fe1ded0
liuyunan <liuyunan.20@gmail.com>
2022-08-11 01:07:27 -0700

    use stack to implement preorder traversal of binary tree

--- core_problem/144_binary_tree_preorder_traversal.py
+++ core_problem/144_binary_tree_preorder_traversal.py
@@ -10,11 +10,25 @@ class TreeNode:
 
 
 class Solution:
-    def preorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+    def preorderTraversal_recursion(self, root: Optional[TreeNode]) -> List[int]:
         tree = []
         if not root:
             return []
         tree.append(root.val)
         tree += self.preorderTraversal(root.left)
         tree += self.preorderTraversal(root.right)
+        return tree
+
+    def preorderTraversal_iteration(self, root: Optional[TreeNode]) -> List[int]:
+        tree = []
+        stack = []
+        while root:
+            tree.append(root.val)
+            if root.right:
+                stack.append(root.right)
+            if root.left:
+                stack.append(root.left)
+            if not stack:
+                break
+            root = stack.pop()
         return tree
