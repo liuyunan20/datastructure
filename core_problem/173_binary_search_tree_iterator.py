c7ef3a9801a87acffa932ea9acd836249ff0923f
liuyunan <liuyunan.20@gmail.com>
2022-08-25 00:21:57 -0700

    add body

--- core_problem/173_binary_search_tree_iterator.py
+++ core_problem/173_binary_search_tree_iterator.py
@@ -1,9 +1,32 @@
 # Definition for a binary tree node.
+from typing import Optional
+
+
 class TreeNode:
     def __init__(self, val=0, left=None, right=None):
         self.val = val
         self.left = left
         self.right = right
 
-        
+
 class BSTIterator:
+    def __init__(self, root: Optional[TreeNode]):
+
+        def build_inorder_tree(root):
+            tree = []
+            if not root:
+                return []
+            tree += build_inorder_tree(root.left)
+            tree.append(root.val)
+            tree += build_inorder_tree(root.right)
+            return tree
+
+        self.tree = build_inorder_tree(root)
+        self.pointer = 0
+
+    def next(self) -> int:
+        self.pointer += 1
+        return self.tree[self.pointer - 1]
+
+    def hasNext(self) -> bool:
+        return self.pointer < len(self.tree)
