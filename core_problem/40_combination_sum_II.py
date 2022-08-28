10b206d90d2cc5a1d75797abf7488ed364d4ff25
liuyunan <liuyunan.20@gmail.com>
2022-08-28 11:49:31 -0700

    skip duplicate in every level loop to avoid duplicate combinations

--- core_problem/40_combination_sum_II.py
+++ core_problem/40_combination_sum_II.py
@@ -18,3 +18,26 @@ class Solution:
 
         dfs([], target, 0)
         return result
+
+    def combinationSum2(self, candidates: List[int], target: int) -> List[List[int]]:
+        result = []
+        candidates.sort()
+
+        def dfs(comb, tgt, idx):
+            if tgt == 0:
+                result.append(list(comb))
+                return
+            if tgt < 0:
+                return
+            for i in range(idx, len(candidates)):
+                # i > idx skip same candidate in this level loop;
+                # if i > 0, skip all duplicate candidates
+                # that's means use one number only once
+                if i > idx and candidates[i] == candidates[i-1]:
+                    continue
+                comb.append(candidates[i])
+                dfs(comb, tgt - candidates[i], i + 1)
+                comb.pop()
+
+        dfs([], target, 0)
+        return result
