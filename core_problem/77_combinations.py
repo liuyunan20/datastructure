c6f58d6293b5758161c97dac9d72837682799c6b
liuyunan <liuyunan.20@gmail.com>
2022-09-13 12:08:12 -0700

    # use index to explore all numbers, append appropriate current list to result         # for each index, the current list could include it or not include

--- core_problem/77_combinations.py
+++ core_problem/77_combinations.py
@@ -15,3 +15,22 @@ class Solution:
 
         backtrack(1, [])
         return result
+
+    def combine_2(self, n: int, k: int) -> List[List[int]]:
+        result = []
+        # use index to explore all numbers, append appropriate current list to result
+        # for each index, the current list could include it or not include
+        def backtrack(i, current):
+            if len(current) == k:
+                result.append(list(current))
+                return
+            if i == n + 1:
+                return
+            current.append(i)
+            backtrack(i + 1, current)
+            current.pop()
+
+            backtrack(i + 1, current)
+
+        backtrack(1, [])
+        return result
