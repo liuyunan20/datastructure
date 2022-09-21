5bf1654df7f275bdbce2f2a820c989a828ab8675
liuyunan <liuyunan.20@gmail.com>
2022-09-20 23:56:33 -0700

    reduce redundant code, use one convert number to save rotate number temporarily

--- main_problem/array_and_matrix/48_rotate_image.py
+++ main_problem/array_and_matrix/48_rotate_image.py
@@ -1,3 +1,6 @@
+from _ast import List
+
+
 class Solution:
     def rotate(self, matrix: List[List[int]]) -> None:
         """
@@ -15,4 +18,19 @@ class Solution:
                 matrix[n - 1 - j][n - 1 - j - i] = converter2
                 matrix[n - 1 - j - i][j] = converter1
                 print(matrix)
+            j += 1
+
+    def rotate_2(self, matrix: List[List[int]]) -> None:
+        """
+        Do not return anything, modify matrix in-place instead.
+        """
+        n = len(matrix)
+        j = 0
+        while n - 2 * j > 1:
+            for i in range(n - 2 * j - 1):
+                converter = matrix[j][j + i]
+                matrix[j][j + i] = matrix[n - 1 - j - i][j]
+                matrix[n - 1 - j - i][j] = matrix[n - 1 - j][n - 1 - j - i]
+                matrix[n - 1 - j][n - 1 - j - i] = matrix[j + i][n - 1 - j]
+                matrix[j + i][n - 1 - j] = converter
             j += 1
