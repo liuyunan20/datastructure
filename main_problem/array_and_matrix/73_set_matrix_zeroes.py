f894e58a03cfb37227d96bed497628885dceb82c
liuyunan <liuyunan.20@gmail.com>
2022-09-24 16:07:46 -0700

    use the first value of every row and column as a flag indicating if there is zero in this row or column

--- main_problem/array_and_matrix/73_set_matrix_zeroes.py
+++ main_problem/array_and_matrix/73_set_matrix_zeroes.py
@@ -1,3 +1,6 @@
+from _ast import List
+
+
 class Solution:
     def setZeroes(self, matrix: List[List[int]]) -> None:
         """
@@ -17,3 +20,32 @@ class Solution:
         for j in zero_columns:
             for i in range(m):
                 matrix[i][j] = 0
+
+    # the first value of every row and column to indicate
+    # if there is zero in this row or column
+    def setZeroes_2(self, matrix: List[List[int]]) -> None:
+        """
+        Do not return anything, modify matrix in-place instead.
+        """
+        m = len(matrix)
+        n = len(matrix[0])
+        first_col_0 = False
+        for i in range(m):
+            if matrix[i][0] == 0:
+                first_col_0 = True
+            for j in range(1, n):
+                if matrix[i][j] == 0:
+                    matrix[i][0] = 0
+                    matrix[0][j] = 0
+        for i in range(1, m):
+            if matrix[i][0] == 0:
+                matrix[i] = [0 for _ in range(n)]
+        for j in range(1, n):
+            if matrix[0][j] == 0:
+                for i in range(m):
+                    matrix[i][j] = 0
+        if matrix[0][0] == 0:
+            matrix[0] = [0 for _ in range(n)]
+        if first_col_0:
+            for i in range(m):
+                matrix[i][0] = 0
