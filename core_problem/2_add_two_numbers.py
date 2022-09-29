45e699bf21e80ff2a84937a4d0cb6be7775019e9
liuyunan <liuyunan.20@gmail.com>
2022-09-29 11:10:07 -0700

    optimize the algorithm, loop until both linked list end. Simulate the ended list node value as 0

--- core_problem/2_add_two_numbers.py
+++ core_problem/2_add_two_numbers.py
@@ -12,7 +12,7 @@ class Solution:
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
         node = ListNode(0)
         result = node
-        helper = 0;
+        helper = 0
         while l1 and l2:
             node.val = (l1.val + l2.val + helper) % 10
             helper = (l1.val + l2.val + helper) // 10
@@ -44,3 +44,20 @@ class Solution:
             pre_node.next = None
 
         return result
+
+    def addTwoNumbers_2(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+        node = ListNode()
+        result = node
+        helper = 0;
+        while l1 or l2 or helper:
+            val1 = l1.val if l1 else 0
+            val2 = l2.val if l2 else 0
+            val = val1 + val2 + helper
+            node.next = ListNode(val % 10)
+            helper = val // 10
+
+            l1 = l1.next if l1 else None
+            l2 = l2.next if l2 else None
+            node = node.next
+
+        return result.next
