cf194f3a5d6f6887ded1d707beae96fedefcf96b
liuyunan <liuyunan.20@gmail.com>
2022-09-30 11:35:05 -0700

    second time to solve this problem and write similar algorithm

--- core_problem/21_merge_two_sorted_lists.py
+++ core_problem/21_merge_two_sorted_lists.py
@@ -1,15 +1,12 @@
 # Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
-from typing import Optional
-
-from datastructure.linked_list_cycle import ListNode
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
 
 
 class Solution:
-    def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
+    def mergeTwoLists(self, list1: ListNode, list2: ListNode) -> ListNode:
         i = list1
         j = list2
         pre = cur = ListNode()
@@ -27,3 +24,18 @@ class Solution:
         else:
             cur.next = j
         return pre.next
+
+    def mergeTwoLists_2(self, list1: ListNode, list2: ListNode) -> ListNode:
+        result = ListNode()
+        node = result
+        while list1 and list2:
+            if list1.val < list2.val:
+                node.next = ListNode(list1.val)
+                list1 = list1.next
+            else:
+                node.next = ListNode(list2.val)
+                list2 = list2.next
+            node = node.next
+
+        node.next = list1 if list1 else list2
+        return result.next
