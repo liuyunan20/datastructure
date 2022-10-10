02cab698913ac30621188b88f094c03b3294960d
liuyunan <liuyunan.20@gmail.com>
2022-10-09 19:44:58 -0700

    minor change

--- core_problem/1_two_sum.py
+++ core_problem/1_two_sum.py
@@ -8,3 +8,7 @@ class Solution:
             if target - num in value_index:
                 return [value_index[target-num], i]
             value_index[num] = i
+
+
+t = Solution()
+print(t.twoSum([4, 2, 7, 11, 15], 9))
