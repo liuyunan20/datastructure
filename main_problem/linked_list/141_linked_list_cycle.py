d8ec090001bb1041509e35a54c1bff5370d1f5d1
liuyunan <liuyunan.20@gmail.com>
2022-10-19 15:30:40 -0700

    use hash table instead of list

--- main_problem/linked_list/141_linked_list_cycle.py
+++ main_problem/linked_list/141_linked_list_cycle.py
@@ -5,11 +5,12 @@ class ListNode:
         self.next = None
 
 class Solution:
-    def hasCycle(self, head: Optional[ListNode]) -> bool:
-        visited = []
+    def hasCycle_2(self, head: Optional[ListNode]) -> bool:
+        # use hash table instead of list
+        visited = set()
         while head:
             if head in visited:
                 return True
-            visited.append(head)
+            visited.add(head)
             head = head.next
         return False
