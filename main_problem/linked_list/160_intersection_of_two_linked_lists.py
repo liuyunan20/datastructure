09b3778f241bf6b806db43bf61c58691e2de227e
liuyunan <liuyunan.20@gmail.com>
2022-10-22 17:10:00 -0700

    # traversal listA then listB head different part, arrive the intersection node a = a.next if a else headB # traversal listB then listA head different part, arrive the intersection node b = b.next if b else headA

--- main_problem/linked_list/160_intersection_of_two_linked_lists.py
+++ main_problem/linked_list/160_intersection_of_two_linked_lists.py
@@ -28,3 +28,13 @@ class Solution:
             if a == b:
                 return a
         return None
+
+    def getIntersectionNode_smart(self, headA: ListNode, headB: ListNode) -> Optional[ListNode]:
+        a = headA
+        b = headB
+        while a != b:
+            # traversal listA then listB head different part, arrive the intersection node
+            a = a.next if a else headB
+            # traversal listB then listA head different part, arrive the intersection node
+            b = b.next if b else headA
+        return a
