30d08c55631502fca631f8715e3b776a8fadf495
liuyunan <liuyunan.20@gmail.com>
2022-10-27 12:53:01 -0700

    Given the head of a singly linked list, return the middle node of the linked list.
    If there are two middle nodes, return the second middle node.
    
    Example 1:
    
    Input: head = [1,2,3,4,5]
    Output: [3,4,5]
    Explanation: The middle node of the list is node 3.
    Example 2:
    
    Input: head = [1,2,3,4,5,6]
    Output: [4,5,6]
    Explanation: Since the list has two middle nodes with values 3 and 4, we return the second one.
    
    Constraints:
    
    The number of nodes in the list is in the range [1, 100].
    1 <= Node.val <= 100

--- main_problem/linked_list/142_linked_list_cycle_II.py
+++ main_problem/linked_list/142_linked_list_cycle_II.py
@@ -3,7 +3,7 @@ class ListNode:
     def __init__(self, x):
         self.val = x
         self.next = None
-        
+
 
 class Solution:
     def detectCycle(self, head: Optional[ListNode]) -> Optional[ListNode]:
