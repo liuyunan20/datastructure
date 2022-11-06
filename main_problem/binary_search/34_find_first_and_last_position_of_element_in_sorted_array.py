09c1e1c1e4d414f1ef908af5a4174da155df25b1
liuyunan <liuyunan.20@gmail.com>
2022-11-06 01:02:05 -0700

    use binary search to find positions of target

--- main_problem/binary_search/34_find_first_and_last_position_of_element_in_sorted_array.py
+++ main_problem/binary_search/34_find_first_and_last_position_of_element_in_sorted_array.py
@@ -8,3 +8,33 @@ class Solution:
         if not positions:
             return [-1, -1]
         return [positions[0], positions[-1]]
+
+    def searchRange_binary_search(self, nums: List[int], target: int) -> List[int]:
+        l = len(nums)
+        if l == 0:
+            return [-1, -1]
+        found = False
+        i = l // 2
+        start = 0
+        end = l - 1
+        while start <= end:
+            if nums[i] == target:
+                found = True
+                break
+            elif nums[i] > target:
+                end = i - 1
+            elif nums[i] < target:
+                start = i + 1
+            i = (end - start) // 2 + start
+        if not found:
+            return [-1, -1]
+        else:
+            j = 0
+            while i + j < l and nums[i + j] == target:
+                j += 1
+            last = i + j - 1
+            j = 0
+            while i - j >= 0 and nums[i - j] == target:
+                j += 1
+            first = i - j + 1
+            return [first, last]
