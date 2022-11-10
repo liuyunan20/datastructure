3b8f3f5dfaf983df6b797e43d598045d987c43b7
liuyunan <liuyunan.20@gmail.com>
2022-11-09 23:52:22 -0800

    can't pass [10,1,10,10,10]

--- main_problem/binary_search/154_find_minimum_in_rotated_sorted_array_II.py
+++ main_problem/binary_search/154_find_minimum_in_rotated_sorted_array_II.py
@@ -1,27 +1,21 @@
-from typing import List
-
-
 class Solution:
     def findMin(self, nums: List[int]) -> int:
         start = 0
         end = len(nums) - 1
+        result = nums[start]
         while start <= end:
             i = (start + end) // 2
             left = nums[start]
             mid = nums[i]
             right = nums[end]
             if left <= mid <= right:
-                return left
+                result = min(result, mid)
+                start += 1
+                end -= 1
+                continue
             if left <= mid:
                 start = i + 1
             if left > mid:
                 end = i
 
-
-if __name__ == '__main__':
-    s = Solution()
-    nums = [3, 4, 5, 1, 2]
-    print(s.findMin(nums))
-
-    nums = [4, 5, 6, 7, 0, 1, 2]
-    print(s.findMin(nums))
+        return result
