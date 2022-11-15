cc421279d3980243d572d99438f2c605014ebab5
liuyunan <liuyunan.20@gmail.com>
2022-11-15 00:00:11 -0800

    use binary search

--- main_problem/binary_search/162_find_peak_element.py
+++ main_problem/binary_search/162_find_peak_element.py
@@ -11,3 +11,21 @@ class Solution:
             if nums[i - 1] < nums[i] and nums[i + 1] < nums[i]:
                 return i
             i += 1
+
+
+    def findPeakElement_binary_search(self, nums: List[int]) -> int:
+        n = len(nums)
+        if n == 1 or nums[0] > nums[1]:
+            return 0
+        if nums[n - 1] > nums[n - 2]:
+            return n - 1
+
+        start = 0
+        end = n - 1
+        while start < end:
+            i = (start + end) // 2
+            if nums[i] < nums[i + 1]:
+                start = i + 1
+            if nums[i] > nums[i + 1]:
+                end = i
+        return start
