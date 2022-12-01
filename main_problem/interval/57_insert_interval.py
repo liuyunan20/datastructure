433f901eb308e7ce17e318b660f9810618be992f
liuyunan <liuyunan.20@gmail.com>
2022-11-30 21:15:06 -0800

    You are given an array of non-overlapping intervals intervals where intervals[i] = [starti, endi] represent the start and the end of the ith interval and intervals is sorted in ascending order by starti. You are also given an interval newInterval = [start, end] that represents the start and end of another interval.
    Insert newInterval into intervals such that intervals is still sorted in ascending order by starti and intervals still does not have any overlapping intervals (merge overlapping intervals if necessary).
    
    Return intervals after the insertion.
    
    Example 1:
    
    Input: intervals = [[1,3],[6,9]], newInterval = [2,5]
    Output: [[1,5],[6,9]]
    Example 2:
    
    Input: intervals = [[1,2],[3,5],[6,7],[8,10],[12,16]], newInterval = [4,8]
    Output: [[1,2],[3,10],[12,16]]
    Explanation: Because the new interval [4,8] overlaps with [3,5],[6,7],[8,10].
    
    Constraints:
    
    0 <= intervals.length <= 104
    intervals[i].length == 2
    0 <= starti <= endi <= 105
    intervals is sorted by starti in ascending order.
    newInterval.length == 2
    0 <= start <= end <= 105

--- main_problem/interval/57_insert_interval.py
+++ main_problem/interval/57_insert_interval.py
@@ -1,3 +1,6 @@
+from typing import List
+
+
 class Solution:
     def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
         n = len(intervals)
@@ -41,4 +44,25 @@ class Solution:
         #             start = intervals[i][0]
         #     end = max(b, intervals[i][1])
         #     result.append([start, end])
+        return result
+
+    def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
+        n = len(intervals)
+        i = 0
+        start, end = newInterval
+        result = []
+        while i < n and start > intervals[i][0]:
+            result.append(intervals[i])
+            i += 1
+        if not result or start > result[-1][1]:
+            result.append(newInterval)
+        else:
+            result[-1][1] = max(end, result[-1][1])
+        while i < n:
+            if result[-1][1] < intervals[i][0]:
+                result.append(intervals[i])
+            else:
+                result[-1][1] = max(intervals[i][1], result[-1][1])
+            i += 1
+
         return result
