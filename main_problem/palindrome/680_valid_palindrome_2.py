f5748c4d1bf189d9764ed04644af5fa5d5f19dff
liuyunan <liuyunan.20@gmail.com>
2022-12-05 20:15:37 -0800

    use helper function to check if a string is a palindrome

--- main_problem/palindrome/680_valid_palindrome_2.py
+++ main_problem/palindrome/680_valid_palindrome_2.py
@@ -10,3 +10,23 @@ class Solution:
                 return True
             i += 1
         return False
+
+    def validPalindrome(self, s: str) -> bool:
+        def check_palindrome(p):
+            i = 0
+            j = len(p) - 1
+            while i < j:
+                if p[i] != p[j]:
+                    return False
+                i += 1
+                j -= 1
+            return True
+
+        i = 0
+        j = len(s) - 1
+        while i < j:
+            if s[i] != s[j]:
+                return check_palindrome(s[i + 1: j + 1]) or check_palindrome(s[i: j])
+            i += 1
+            j -= 1
+        return True
