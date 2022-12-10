cda0349ab3c0f8bf9801ea3c561b448c0931d7c8
liuyunan <liuyunan.20@gmail.com>
2022-12-09 22:55:59 -0800

    use two pointers

--- main_problem/multi_pointer/slide_window/209_minimum_size_subarray_sum.py
+++ main_problem/multi_pointer/slide_window/209_minimum_size_subarray_sum.py
@@ -15,3 +15,23 @@ class Solution:
                 result = min(result, j - i + 1)
                 i += 1
         return result
+
+    def minSubArrayLen(self, target: int, nums: List[int]) -> int:
+        if sum(nums) < target:
+            return 0
+        i = 0
+        j = 0
+        n = len(nums)
+        result = n
+        sub_sum = 0
+        while j <= n:
+            if sub_sum < target and j < n:
+                sub_sum += nums[j]
+                j += 1
+            elif sub_sum >= target:
+                result = min(result, j - i)
+                sub_sum -= nums[i]
+                i += 1
+            else:
+                break
+        return result
