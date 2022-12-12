2c20ee2f005f8453794d56a9717536c07ef5b493
liuyunan <liuyunan.20@gmail.com>
2022-12-11 23:37:19 -0800

    tle for test case descending order with window length 10000

--- main_problem/multi_pointer/slide_window/239_sliding_window_maximum.py
+++ main_problem/multi_pointer/slide_window/239_sliding_window_maximum.py
@@ -8,3 +8,19 @@ class Solution:
         for i in range(n - k + 1):
             result.append(max(nums[i: i + k]))
         return result
+
+    def maxSlidingWindow_tle2(self, nums: List[int], k: int) -> List[int]:
+        result = []
+        n = len(nums)
+        max_num = max(nums[:k])
+        result.append(max_num)
+        for i in range(k, n):
+            if nums[i] >= max_num:
+                result.append(nums[i])
+                max_num = nums[i]
+            elif nums[i - k] == max_num:
+                max_num = max(nums[i - k + 1: i + 1])
+                result.append(max_num)
+            else:
+                result.append(max_num)
+        return result
