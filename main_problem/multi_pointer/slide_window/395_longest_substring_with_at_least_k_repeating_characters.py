43451b9c6429ba28dd8f0f3d7cec56f4786c9118
liuyunan <liuyunan.20@gmail.com>
2022-12-16 23:00:19 -0800

    can't decide how to justify window

--- main_problem/multi_pointer/slide_window/395_longest_substring_with_at_least_k_repeating_characters.py
+++ main_problem/multi_pointer/slide_window/395_longest_substring_with_at_least_k_repeating_characters.py
@@ -24,3 +24,35 @@ class Solution:
                     del freq[s[j]]
                 j -= 1
         return 0
+
+    def longestSubstring_wrong2(self, s: str, k: int) -> int:
+        if len(s) == 1 and k == 1:
+            return 1
+        freq = {}
+        letter_more = set()
+        for letter in s:
+            freq.setdefault(letter, 0)
+            freq[letter] += 1
+            if freq[letter] >= k:
+                letter_more.add(letter)
+        letter_less = set(list(s)) - letter_more
+
+        i = 0
+        n = len(s)
+        j = n - 1
+        while i < j:
+            if min(freq.values()) >= k:
+                return j - i + 1
+            if freq[s[i]] < k:
+                freq[s[i]] -= 1
+                if freq[s[i]] == 0:
+                    del freq[s[i]]
+                i += 1
+            elif freq[s[j]] < k:
+                freq[s[j]] -= 1
+                if freq[s[j]] == 0:
+                    del freq[s[j]]
+                j -= 1
+            else:
+                i =
+        return 0
