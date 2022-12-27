7dc8f96b195dc535b7b35f5030016c908891a64c
liuyunan <liuyunan.20@gmail.com>
2022-12-26 20:49:50 -0800

    solve tle

--- main_problem/multi_pointer/528_random_pick_with_weight.py
+++ main_problem/multi_pointer/528_random_pick_with_weight.py
@@ -1,13 +1,14 @@
+import random
 class Solution:
 
     def __init__(self, w: List[int]):
         self.w = w
+        self.index = [i for i in range(len(w))]
 
     def pickIndex(self) -> int:
         if len(self.w) == 1:
             return 0
-        w_sum = sum(self.w)
-        return random.choices([i for i in range(len(self.w))], weights=self.w, k=1)[0]
+        return random.choices(self.index, weights=self.w, k=1)[0]
 
 
 # Your Solution object will be instantiated and called as such:
