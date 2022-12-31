7f7d5bdd3b85d5cbcfabc7072f05df0fc5142e1b
liuyunan <liuyunan.20@gmail.com>
2022-12-31 00:48:50 -0800

    if reach the sum greater than target, end current loop

--- main_problem/multi_pointer/sum_and_difference/259_3sum_smaller.py
+++ main_problem/multi_pointer/sum_and_difference/259_3sum_smaller.py
@@ -12,3 +12,16 @@ class Solution:
                     if nums[i] + nums[j] + nums[k] < target:
                         result += 1
         return result
+
+    def threeSumSmaller(self, nums: List[int], target: int) -> int:
+        nums.sort()
+        n = len(nums)
+        result = 0
+        for i in range(n):
+            for j in range(i + 1, n):
+                for k in range(j + 1, n):
+                    if nums[i] + nums[j] + nums[k] < target:
+                        result += 1
+                    else:
+                        break
+        return result
