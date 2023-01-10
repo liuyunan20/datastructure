7d2e88b7dbbfbface5b2fb4e1e92eda186be8053
liuyunan <liuyunan.20@gmail.com>
2023-01-09 23:59:15 -0800

    unknown solution

---
+++ main_problem/BSF/maze/490_the_maze.py
@@ -0,0 +1,59 @@
+from typing import List
+
+
+class Solution:
+    def hasPath(self, maze: List[List[int]], start: List[int], destination: List[int]) -> bool:
+        m = len(maze)
+        n = len(maze[0])
+        directions = {}
+        for i in range(m):
+            for j in range(n):
+                directions[(i, j)] = set()
+
+        def find_direction(i, j):
+            d = []
+            if i + 1 < m and maze[i + 1][j] == 0 and "down" not in directions[(i, j)]:
+                d.append("down")
+            if i - 1 >= 0 and maze[i - 1][j] == 0 and "up" not in directions[(i, j)]:
+                d.append("up")
+            if j + 1 < n and maze[i][j + 1] == 0 and "right" not in directions[(i, j)]:
+                d.append("right")
+            if j - 1 >= 0 and maze[i][j - 1] == 0 and "left" not in directions[(i, j)]:
+                d.append("left")
+            return d
+
+        def move(now, direction):
+            i, j = now
+            if direction == "left":
+                while j >= 0 and maze[i][j] == 0:
+                    j -= 1
+                directions[(i, j + 1)].add("right")
+                return [i, j + 1]
+            if direction == "right":
+                while j < n and maze[i][j] == 0:
+                    j += 1
+                directions[(i, j - 1)].add("left")
+                return [i, j - 1]
+            if direction == "up":
+                while i >= 0 and maze[i][j] == 0:
+                    i -= 1
+                directions[(i + 1, j)].add("down")
+                return [i + 1, j]
+            if direction == "down":
+                while i < m and maze[i][j] == 0:
+                    i += 1
+                directions[(i - 1, j)].add("up")
+                return [i - 1, j]
+
+        queue = [start]
+        while queue:
+            i, j = queue.pop(0)
+            ds = find_direction(i, j)
+            if not ds:
+                return False
+            for d in ds:
+                stop = move((i, j), d)
+                if stop == destination:
+                    return True
+                queue.append(stop)
+        return False
