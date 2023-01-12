28c1c3ae2b379b71790e8e9ceeb29b28b5fccae7
liuyunan <liuyunan.20@gmail.com>
2023-01-11 23:33:00 -0800

    There is a ball in a maze with empty spaces (represented as 0) and walls (represented as 1). The ball can go through the empty spaces by rolling up, down, left or right, but it won't stop rolling until hitting a wall. When the ball stops, it could choose the next direction.
    Given the m x n maze, the ball's start position and the destination, where start = [startrow, startcol] and destination = [destinationrow, destinationcol], return the shortest distance for the ball to stop at the destination. If the ball cannot stop at destination, return -1.
    
    The distance is the number of empty spaces traveled by the ball from the start position (excluded) to the destination (included).
    
    You may assume that the borders of the maze are all walls (see examples).
    
    Example 1:
    
    Input: maze = [[0,0,1,0,0],[0,0,0,0,0],[0,0,0,1,0],[1,1,0,1,1],[0,0,0,0,0]], start = [0,4], destination = [4,4]
    Output: 12
    Explanation: One possible way is : left -> down -> left -> down -> right -> down -> right.
    The length of the path is 1 + 1 + 3 + 1 + 2 + 2 + 2 = 12.
    Example 2:
    
    Input: maze = [[0,0,1,0,0],[0,0,0,0,0],[0,0,0,1,0],[1,1,0,1,1],[0,0,0,0,0]], start = [0,4], destination = [3,2]
    Output: -1
    Explanation: There is no way for the ball to stop at the destination. Notice that you can pass through the destination but you cannot stop there.
    Example 3:
    
    Input: maze = [[0,0,0,0,0],[1,1,0,0,1],[0,0,0,0,0],[0,1,0,0,1],[0,1,0,0,0]], start = [4,3], destination = [0,1]
    Output: -1
    
    Constraints:
    
    m == maze.length
    n == maze[i].length
    1 <= m, n <= 100
    maze[i][j] is 0 or 1.
    start.length == 2
    destination.length == 2
    0 <= startrow, destinationrow < m
    0 <= startcol, destinationcol < n
    Both the ball and the destination exist in an empty space, and they will not be in the same position initially.
    The maze contains at least 2 empty spaces.

--- main_problem/BSF/matix/505_the_maze_II.py
+++ main_problem/BSF/matix/505_the_maze_II.py
@@ -2,72 +2,50 @@ from typing import List
 
 
 class Solution:
-    def shortestDistance(self, maze: List[List[int]], start: List[int], destination: List[int]) -> int:
+
+    def shortestDistance_bfs(self, maze: List[List[int]], start: List[int], destination: List[int]) -> int:
         m = len(maze)
         n = len(maze[0])
-        directions = {}
-        for i in range(m):
-            for j in range(n):
-                directions[(i, j)] = set()
-
-        def find_direction(i, j):
-            d = []
-            if i + 1 < m and maze[i + 1][j] == 0 and "down" not in directions[(i, j)]:
-                d.append("down")
-            if i - 1 >= 0 and maze[i - 1][j] == 0 and "up" not in directions[(i, j)]:
-                d.append("up")
-            if j + 1 < n and maze[i][j + 1] == 0 and "right" not in directions[(i, j)]:
-                d.append("right")
-            if j - 1 >= 0 and maze[i][j - 1] == 0 and "left" not in directions[(i, j)]:
-                d.append("left")
-            return d
 
         def move(now, direction):
             i, j = now
-            directions[(i, j)].add(direction)
+            count = 0
             if direction == "left":
                 while j >= 0 and maze[i][j] == 0:
                     j -= 1
-                directions[(i, j + 1)].add("right")
-                return [i, j + 1], now[1] - j - 1
+                    count += 1
+                return [i, j + 1], count - 1
             if direction == "right":
                 while j < n and maze[i][j] == 0:
                     j += 1
-                directions[(i, j - 1)].add("left")
-                return [i, j - 1], j - 1 - now[1]
+                    count += 1
+                return [i, j - 1], count - 1
             if direction == "up":
                 while i >= 0 and maze[i][j] == 0:
                     i -= 1
-                directions[(i + 1, j)].add("down")
-                return [i + 1, j], now[0] - i - 1
+                    count += 1
+                return [i + 1, j], count - 1
             if direction == "down":
                 while i < m and maze[i][j] == 0:
                     i += 1
-                directions[(i - 1, j)].add("up")
-                return [i - 1, j], i - 1 - now[0]
+                    count += 1
+                return [i - 1, j], count - 1
 
-        queue = [start]
-        steps = []
-        position_step = {tuple(start): [0]}
+        queue = [(start, 0)]
+        position_step = {}
         while queue:
-            p = queue.pop(0)
-            i, j = p
-            ds = find_direction(i, j)
-            if not ds:
-                continue
-            for d in ds:
-                cur_step = min(position_step[tuple(p)])
-                stop, mov_step = move(p, d)
-                cur_step += mov_step
-                if stop == destination:
-                    steps.append(cur_step)
-                    print(steps)
+            l = len(queue)
+            while l > 0:
+                l -= 1
+                p, dist = queue.pop(0)
+                if tuple(p) in position_step and position_step[tuple(p)] <= dist:
                     continue
-                i, j = stop
-                if find_direction(i, j):
-                    queue.append(stop)
-                    position_step.setdefault((i, j), []).append(cur_step)
-
-        if steps:
-            return min(steps)
+                position_step[tuple(p)] = dist
+                for d in ["down", "up", "left", "right"]:
+                    cur_step = position_step[tuple(p)]
+                    stop, mov_step = move(p, d)
+                    cur_step += mov_step
+                    queue.append((stop, cur_step))
+        if tuple(destination) in position_step:
+            return position_step[tuple(destination)]
         return -1
