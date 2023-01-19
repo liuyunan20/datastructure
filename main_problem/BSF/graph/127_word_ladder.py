9e6633f0a8e5ad4d1625af6f7540aa84fddeb662
liuyunan <liuyunan.20@gmail.com>
2023-01-19 00:07:42 -0800

    A transformation sequence from word beginWord to word endWord using a dictionary wordList is a sequence of words beginWord -> s1 -> s2 -> ... -> sk such that:
    Every adjacent pair of words differs by a single letter.
    Every si for 1 <= i <= k is in wordList. Note that beginWord does not need to be in wordList.
    sk == endWord
    Given two words, beginWord and endWord, and a dictionary wordList, return the number of words in the shortest transformation sequence from beginWord to endWord, or 0 if no such sequence exists.
    
    Example 1:
    
    Input: beginWord = "hit", endWord = "cog", wordList = ["hot","dot","dog","lot","log","cog"]
    Output: 5
    Explanation: One shortest transformation sequence is "hit" -> "hot" -> "dot" -> "dog" -> cog", which is 5 words long.
    Example 2:
    
    Input: beginWord = "hit", endWord = "cog", wordList = ["hot","dot","dog","lot","log"]
    Output: 0
    Explanation: The endWord "cog" is not in wordList, therefore there is no valid transformation sequence.
    
    Constraints:
    
    1 <= beginWord.length <= 10
    endWord.length == beginWord.length
    1 <= wordList.length <= 5000
    wordList[i].length == beginWord.length
    beginWord, endWord, and wordList[i] consist of lowercase English letters.
    beginWord != endWord
    All the words in wordList are unique.

--- main_problem/BSF/graph/127_word_ladder.py
+++ main_problem/BSF/graph/127_word_ladder.py
@@ -1,38 +1,24 @@
+import string
 from typing import List
 
 
 class Solution:
-    def ladderLength_tle(self, beginWord: str, endWord: str, wordList: List[str]) -> int:
-        def check_neighbor(w1, w2):
-            diff = 0
-            for l1, l2 in zip(w1, w2):
-                if l1 != l2:
-                    diff += 1
-            if diff == 1:
-                return True
-            return False
-
-        word_neighbor = {beginWord: set()}
-        for word in wordList:
-            if check_neighbor(beginWord, word):
-                word_neighbor[beginWord].add(word)
-            word_neighbor[word] = set()
-            for word2 in wordList:
-                if check_neighbor(word, word2):
-                    word_neighbor[word].add(word2)
-
-        visited = set()
+    def ladderLength(self, beginWord: str, endWord: str, wordList: List[str]) -> int:
+        wordList = set(wordList)
         queue = [beginWord]
         step = 1
+        word_length = len(beginWord)
         while queue:
             step += 1
             l = len(queue)
             for _ in range(l):
                 cur_word = queue.pop(0)
-                for next_word in word_neighbor[cur_word]:
-                    if next_word == endWord:
-                        return step
-                    if next_word not in visited:
-                        queue.append(next_word)
-                        visited.add(next_word)
+                for i in range(word_length):
+                    for letter in string.ascii_lowercase:
+                        next_word = cur_word[:i] + letter + cur_word[i + 1:]
+                        if next_word in wordList:
+                            if next_word == endWord:
+                                return step
+                            queue.append(next_word)
+                            wordList.discard(next_word)
         return 0
