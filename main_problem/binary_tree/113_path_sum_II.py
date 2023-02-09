7eb8129b1bf455200ab26228e246b2b60c85c7bf
liuyunan <liuyunan.20@gmail.com>
2023-02-08 20:04:13 -0800

    don't copy path to next level recursion, add copy when find correct path, then pop out every recursion

--- main_problem/binary_tree/113_path_sum_II.py
+++ main_problem/binary_tree/113_path_sum_II.py
@@ -15,15 +15,15 @@ class Solution:
             if not node.left and not node.right:
                 if node.val == target:
                     path.append(node.val)
-                    result.append(path)
+                    result.append(path[::])
+                    path.pop()
                 return
             else:
                 target -= node.val
                 path.append(node.val)
-                new_path = [x for x in path]
-                helper(node.left, target, new_path)
-                new_path = [x for x in path]
-                helper(node.right, target, new_path)
+                helper(node.left, target, path)
+                helper(node.right, target, path)
+                path.pop()
 
         helper(root, targetSum, [])
         return result
