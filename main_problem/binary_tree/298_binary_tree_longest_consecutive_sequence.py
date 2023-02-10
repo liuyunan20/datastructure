074a4827e2dae84407b29d81fafd7b1a4e63a4c3
liuyunan <liuyunan.20@gmail.com>
2023-02-09 23:31:52 -0800

    Given the root of a binary tree, return the length of the longest consecutive sequence path.
    A consecutive sequence path is a path where the values increase by one along the path.
    
    Note that the path can start at any node in the tree, and you cannot go from a node to its parent in the path.
    
    Example 1:
    
    Input: root = [1,null,3,2,4,null,null,null,5]
    Output: 3
    Explanation: Longest consecutive sequence path is 3-4-5, so return 3.
    Example 2:
    
    Input: root = [2,null,3,2,null,1]
    Output: 2
    Explanation: Longest consecutive sequence path is 2-3, not 3-2-1, so return 2.
    
    Constraints:
    
    The number of nodes in the tree is in the range [1, 3 * 104].
    -3 * 104 <= Node.val <= 3 * 104

--- main_problem/binary_tree/298_binary_tree_longest_consecutive_sequence.py
+++ main_problem/binary_tree/298_binary_tree_longest_consecutive_sequence.py
@@ -6,26 +6,20 @@ class TreeNode:
 
 
 class Solution:
-    def longestConsecutive_tle(self, root: Optional[TreeNode]) -> int:
+    def longestConsecutive(self, root: Optional[TreeNode]) -> int:
+
         def find_cons_path(node, path):
             if not node:
                 return path
             if node.left and node.left.val - node.val == 1:
                 path_left = find_cons_path(node.left, path + 1)
             else:
-                path_left = path
+                path_left = find_cons_path(node.left, 1)
             if node.right and node.right.val - node.val == 1:
                 path_right = find_cons_path(node.right, path + 1)
             else:
-                path_right = path
-            return max(path, path_left, path_right)
-        queue = [root]
-        result = 1
-        while queue:
-            n = queue.pop(0)
-            result = max(result, find_cons_path(n, 1))
-            if n.left:
-                queue.append(n.left)
-            if n.right:
-                queue.append(n.right)
-        return result
+                path_right = find_cons_path(node.right, 1)
+            path = max(path, path_left, path_right)
+            return path
+
+        return find_cons_path(root, 1)
