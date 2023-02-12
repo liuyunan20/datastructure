f72fa11767d1b8ac2d5b9f480123e137a4c66316
liuyunan <liuyunan.20@gmail.com>
2023-02-11 18:27:07 -0800

    Given a binary tree, find the lowest common ancestor (LCA) of two given nodes in the tree.
    According to the definition of LCA on Wikipedia: “The lowest common ancestor is defined between two nodes p and q as the lowest node in T that has both p and q as descendants (where we allow a node to be a descendant of itself).”
    
    Example 1:
    
    Input: root = [3,5,1,6,2,0,8,null,null,7,4], p = 5, q = 1
    Output: 3
    Explanation: The LCA of nodes 5 and 1 is 3.
    Example 2:
    
    Input: root = [3,5,1,6,2,0,8,null,null,7,4], p = 5, q = 4
    Output: 5
    Explanation: The LCA of nodes 5 and 4 is 5, since a node can be a descendant of itself according to the LCA definition.
    Example 3:
    
    Input: root = [1,2], p = 1, q = 2
    Output: 1
    
    Constraints:
    
    The number of nodes in the tree is in the range [2, 105].
    -109 <= Node.val <= 109
    All Node.val are unique.
    p != q
    p and q will exist in the tree.

--- main_problem/binary_tree/236_lowest_common_ancestor_of_a_binary_tree.py
+++ main_problem/binary_tree/236_lowest_common_ancestor_of_a_binary_tree.py
@@ -5,7 +5,7 @@ class TreeNode:
         self.right = None
 
 class Solution:
-    def lowestCommonAncestor_tle(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
         def find_p(node, target):
             if not node:
                 return False
@@ -13,11 +13,41 @@ class Solution:
                 return True
             return find_p(node.left, target) or find_p(node.right, target)
 
-        node = root
-        while node:
-            if find_p(node.left, p) and find_p(node.left, q):
-                node = node.left
-            elif find_p(node.right, p) and find_p(node.right, q):
-                node = node.right
+        child_parent = {}
+        nodes = [root]
+        p_parents = []
+        q_parents = []
+        while nodes:
+            node = nodes.pop(0)
+            if node == p and find_p(node, q):
+                return p
+            if node == q and find_p(node, p):
+                return q
+            if node == p:
+                while p != root:
+                    p_parents.append(child_parent[p])
+                    p = child_parent[p]
+            if node == q:
+                while q != root:
+                    q_parents.append(child_parent[q])
+                    q = child_parent[q]
+            if p == root and q == root:
+                break
+            if node.left:
+                child_parent[node.left] = node
+                nodes.append(node.left)
+            if node.right:
+                child_parent[node.right] = node
+                nodes.append(node.right)
+        result = root
+        print(p_parents)
+        print(q_parents)
+        while True:
+            if not p_parents or not q_parents:
+                return result
+            p = p_parents.pop()
+            q = q_parents.pop()
+            if p != q:
+                return result
             else:
-                return node
+                result = p
