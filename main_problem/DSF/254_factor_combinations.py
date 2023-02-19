96820edb96ac896e71f7d90a622df28d2b8a3e41
liuyunan <liuyunan.20@gmail.com>
2023-02-18 21:40:13 -0800

    still tle

--- main_problem/DSF/254_factor_combinations.py
+++ main_problem/DSF/254_factor_combinations.py
@@ -2,16 +2,15 @@ from typing import List
 
 
 class Solution:
-    def getFactors_tle(self, n: int) -> List[List[int]]:
+    def getFactors(self, n: int) -> List[List[int]]:
         if n < 2:
             return []
         result = []
-
         def dfs(comb, idx, prod):
             if prod == 1:
                 result.append(list(comb))
                 return
-            if prod < 1:
+            if prod // 1 != prod:
                 return
             for i in range(idx, n // 2 + 1):
                 comb.append(i)
