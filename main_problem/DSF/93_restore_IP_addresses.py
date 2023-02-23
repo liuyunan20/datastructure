102cfa978afe1ae4584e17a29439dd0b2305b2d9
liuyunan <liuyunan.20@gmail.com>
2023-02-22 22:06:09 -0800

    A valid IP address consists of exactly four integers separated by single dots. Each integer is between 0 and 255 (inclusive) and cannot have leading zeros.
    For example, "0.1.2.201" and "192.168.1.1" are valid IP addresses, but "0.011.255.245", "192.168.1.312" and "192.168@1.1" are invalid IP addresses.
    Given a string s containing only digits, return all possible valid IP addresses that can be formed by inserting dots into s. You are not allowed to reorder or remove any digits in s. You may return the valid IP addresses in any order.
    
    Example 1:
    
    Input: s = "25525511135"
    Output: ["255.255.11.135","255.255.111.35"]
    Example 2:
    
    Input: s = "0000"
    Output: ["0.0.0.0"]
    Example 3:
    
    Input: s = "101023"
    Output: ["1.0.10.23","1.0.102.3","10.1.0.23","10.10.2.3","101.0.2.3"]
    
    Constraints:
    
    1 <= s.length <= 20
    s consists of digits only.

--- main_problem/DSF/93_restore_IP_addresses.py
+++ main_problem/DSF/93_restore_IP_addresses.py
@@ -1,36 +1,35 @@
+from typing import List
+
+
 class Solution:
     def restoreIpAddresses(self, s: str) -> List[str]:
         n = len(s)
+        result = []
 
         def valid_addr(positions):
+            ip = ""
             for i, p in enumerate(positions):
-                print(positions)
                 if i == 0:
                     num = s[:p + 1]
                 else:
                     num = s[positions[i - 1] + 1: p + 1]
-                print(num)
+                ip += num + "."
                 if (num.startswith("0") and len(num) > 1) or (num and int(num) > 255):
                     return False
-
+            result.append(ip.rstrip("."))
             return True
-
-        result = []
 
         def dfs(comb, idx):
             if idx == n:
                 return
             if len(comb) == 3:
                 comb.append(n - 1)
-                if valid_addr(comb):
-                    result.append(list(comb))
+                valid_addr(comb)
                 comb.pop()
                 return
             for i in range(idx, n - 1):
                 comb.append(i)
                 dfs(comb, i + 1)
                 comb.pop()
-
         dfs([], 0)
-
         return result
