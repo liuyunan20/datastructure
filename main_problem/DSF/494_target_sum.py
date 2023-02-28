8656df179fe35407e6b3ca374203702c018dfe9b
liuyunan <liuyunan.20@gmail.com>
2023-02-27 22:27:00 -0800

    You are given an integer array nums and an integer target.
    You want to build an expression out of nums by adding one of the symbols '+' and '-' before each integer in nums and then concatenate all the integers.
    
    For example, if nums = [2, 1], you can add a '+' before 2 and a '-' before 1 and concatenate them to build the expression "+2-1".
    Return the number of different expressions that you can build, which evaluates to target.
    
    Example 1:
    
    Input: nums = [1,1,1,1,1], target = 3
    Output: 5
    Explanation: There are 5 ways to assign symbols to make the sum of nums be target 3.
    -1 + 1 + 1 + 1 + 1 = 3
    +1 - 1 + 1 + 1 + 1 = 3
    +1 + 1 - 1 + 1 + 1 = 3
    +1 + 1 + 1 - 1 + 1 = 3
    +1 + 1 + 1 + 1 - 1 = 3
    Example 2:
    
    Input: nums = [1], target = 1
    Output: 1
    
    Constraints:
    
    1 <= nums.length <= 20
    0 <= nums[i] <= 1000
    0 <= sum(nums[i]) <= 1000
    -1000 <= target <= 1000

--- main_problem/DSF/494_target_sum.py
+++ main_problem/DSF/494_target_sum.py
@@ -2,16 +2,20 @@ from typing import List
 
 
 class Solution:
-    def findTargetSumWays_tle(self, nums: List[int], target: int) -> int:
+    def findTargetSumWays(self, nums: List[int], target: int) -> int:
         n = len(nums)
-        result = [0]
+        i_tg_result = {}
 
         def dfs(i, tg):
             if i == n:
                 if tg == 0:
-                    result[0] += 1
-                return
-            dfs(i + 1, tg + nums[i])
-            dfs(i + 1, tg - nums[i])
+                    return 1
+                return 0
+            if (i, tg) in i_tg_result:
+                return i_tg_result[(i, tg)]
+            a = dfs(i + 1, tg + nums[i])
+            b = dfs(i + 1, tg - nums[i])
+            i_tg_result[(i, tg)] = a + b
+            return i_tg_result[(i, tg)]
         dfs(0, target)
-        return result[0]
+        return i_tg_result[(0, target)]
