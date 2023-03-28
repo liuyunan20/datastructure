958ddf53364535b7f40e18c802e14673195bb1d7
liuyunan <liuyunan.20@gmail.com>
2023-03-27 21:46:54 -0700

    Given the head of a singly linked list, return the middle node of the linked list.
    If there are two middle nodes, return the second middle node.
    
    Example 1:
    
    Input: head = [1,2,3,4,5]
    Output: [3,4,5]
    Explanation: The middle node of the list is node 3.
    Example 2:
    
    Input: head = [1,2,3,4,5,6]
    Output: [4,5,6]
    Explanation: Since the list has two middle nodes with values 3 and 4, we return the second one.
    
    Constraints:
    
    The number of nodes in the list is in the range [1, 100].
    1 <= Node.val <= 100

---
+++ main_problem/datastructure/linked_list/876_middle_of_the_linked_list.py
@@ -0,0 +1,16 @@
+from typing import Optional
+
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+class Solution:
+    def middleNode(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        nodes = []
+        while head:
+            nodes.append(head)
+            head = head.next
+        return nodes[len(nodes)//2]
