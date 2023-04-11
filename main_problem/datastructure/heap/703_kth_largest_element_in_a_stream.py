56304668a2769a3abcd75dc4ce38af027ca72da4
liuyunan <liuyunan.20@gmail.com>
2023-04-10 21:30:57 -0700

    use heapq

--- main_problem/datastructure/heap/703_kth_largest_element_in_a_stream.py
+++ main_problem/datastructure/heap/703_kth_largest_element_in_a_stream.py
@@ -1,14 +1,19 @@
+import heapq
+from typing import List
+
+
 class KthLargest:
 
     def __init__(self, k: int, nums: List[int]):
         self.k = k
         self.nums = nums
+        heapq.heapify(self.nums)
 
     def add(self, val: int) -> int:
-        self.nums.append(val)
-        self.nums.sort()
-        return self.nums[-self.k]
-
+        heapq.heappush(self.nums, val)
+        while len(self.nums) > self.k:
+            heapq.heappop(self.nums)
+        return self.nums[0]
 
 # Your KthLargest object will be instantiated and called as such:
 # obj = KthLargest(k, nums)
