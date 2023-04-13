10a6677f217957079b9aef1ab6c73a89760fd56c
liuyunan <liuyunan.20@gmail.com>
2023-04-12 20:51:59 -0700

    still tle

--- main_problem/datastructure/heap/480_sliding_window_median.py
+++ main_problem/datastructure/heap/480_sliding_window_median.py
@@ -3,12 +3,10 @@ from typing import List
 
 
 class Solution:
-    def medianSlidingWindow(self, nums: List[int], k: int) -> List[float]:
+    def medianSlidingWindow_tle(self, nums: List[int], k: int) -> List[float]:
         result = []
-        window = nums[:k - 1]
-        heapq.heapify(window)
-        for i in range(k - 1, len(nums)):
-            heapq.heappush(window, nums[i])
+        for i in range(len(nums) - k):
+            window = nums[i: i + k]
             if k % 2 == 1:
                 n = k // 2 + 1
                 result.append(heapq.nsmallest(n, window)[-1])
@@ -16,5 +14,4 @@ class Solution:
                 n = k // 2
                 l = heapq.nsmallest(n + 1, window)
                 result.append((l[-1] + l[-2]) / 2)
-            window.remove(nums[i + 1 - k])
         return result
