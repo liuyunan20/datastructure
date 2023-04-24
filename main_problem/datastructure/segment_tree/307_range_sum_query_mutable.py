8eb641ef718bbbcc98027f1f19f61590881aa766
liuyunan <liuyunan.20@gmail.com>
2023-04-23 21:18:12 -0700

    tle in sumRange

---
+++ main_problem/datastructure/segment_tree/307_range_sum_query_mutable.py
@@ -0,0 +1,19 @@
+from typing import List
+
+
+class NumArray:
+
+    def __init__(self, nums: List[int]):
+        self.nums = nums
+
+    def update(self, index: int, val: int) -> None:
+        self.nums[index] = val
+
+    def sumRange_tle(self, left: int, right: int) -> int:
+        return sum(self.nums[left: right + 1])
+
+
+# Your NumArray object will be instantiated and called as such:
+# obj = NumArray(nums)
+# obj.update(index,val)
+# param_2 = obj.sumRange(left,right)
