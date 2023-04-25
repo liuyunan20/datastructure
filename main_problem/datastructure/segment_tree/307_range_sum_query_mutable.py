42c3d0869ed183da53c0374cfd6489ac209a1269
liuyunan <liuyunan.20@gmail.com>
2023-04-24 23:04:40 -0700

    Given an integer array nums, handle multiple queries of the following types:
    Update the value of an element in nums.
    Calculate the sum of the elements of nums between indices left and right inclusive where left <= right.
    Implement the NumArray class:
    
    NumArray(int[] nums) Initializes the object with the integer array nums.
    void update(int index, int val) Updates the value of nums[index] to be val.
    int sumRange(int left, int right) Returns the sum of the elements of nums between indices left and right inclusive (i.e. nums[left] + nums[left + 1] + ... + nums[right]).
    
    Example 1:
    
    Input
    ["NumArray", "sumRange", "update", "sumRange"]
    [[[1, 3, 5]], [0, 2], [1, 2], [0, 2]]
    Output
    [null, 9, null, 8]
    
    Explanation
    NumArray numArray = new NumArray([1, 3, 5]);
    numArray.sumRange(0, 2); // return 1 + 3 + 5 = 9
    numArray.update(1, 2);   // nums = [1, 2, 5]
    numArray.sumRange(0, 2); // return 1 + 2 + 5 = 8
    
    Constraints:
    
    1 <= nums.length <= 3 * 104
    -100 <= nums[i] <= 100
    0 <= index < nums.length
    -100 <= val <= 100
    0 <= left <= right < nums.length
    At most 3 * 104 calls will be made to update and sumRange.

--- main_problem/datastructure/segment_tree/307_range_sum_query_mutable.py
+++ main_problem/datastructure/segment_tree/307_range_sum_query_mutable.py
@@ -5,21 +5,18 @@ class NumArray:
 
     def __init__(self, nums: List[int]):
         self.nums = nums
-        self.sums = {-1: 0}
-        self.length = len(nums)
-        sub = 0
-        for i in range(len(self.nums)):
-            self.sums[i] = sub + self.nums[i]
-            sub = self.sums[i]
+        self.sum = sum(nums)
 
-    def update_tle(self, index: int, val: int) -> None:
+    def update(self, index: int, val: int) -> None:
         d = val - self.nums[index]
         self.nums[index] = val
-        for i in range(index, self.length):
-            self.sums[i] += d
+        self.sum += d
 
     def sumRange(self, left: int, right: int) -> int:
-        return self.sums[right] - self.sums[left - 1]
+        if right - left < len(self.nums) // 2:
+            return sum(self.nums[left: right + 1])
+        else:
+            return self.sum - sum(self.nums[:left]) - sum(self.nums[right + 1:])
 
 
 
