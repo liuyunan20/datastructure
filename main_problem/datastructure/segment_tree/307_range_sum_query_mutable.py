9a297294d91420d04341ad349e696557dffb6e2b
liuyunan <liuyunan.20@gmail.com>
2023-04-24 22:54:23 -0700

    changed update, still tle

--- main_problem/datastructure/segment_tree/307_range_sum_query_mutable.py
+++ main_problem/datastructure/segment_tree/307_range_sum_query_mutable.py
@@ -5,12 +5,22 @@ class NumArray:
 
     def __init__(self, nums: List[int]):
         self.nums = nums
+        self.sums = {-1: 0}
+        self.length = len(nums)
+        sub = 0
+        for i in range(len(self.nums)):
+            self.sums[i] = sub + self.nums[i]
+            sub = self.sums[i]
 
-    def update(self, index: int, val: int) -> None:
+    def update_tle(self, index: int, val: int) -> None:
+        d = val - self.nums[index]
         self.nums[index] = val
+        for i in range(index, self.length):
+            self.sums[i] += d
 
-    def sumRange_tle(self, left: int, right: int) -> int:
-        return sum(self.nums[left: right + 1])
+    def sumRange(self, left: int, right: int) -> int:
+        return self.sums[right] - self.sums[left - 1]
+
 
 
 # Your NumArray object will be instantiated and called as such:
