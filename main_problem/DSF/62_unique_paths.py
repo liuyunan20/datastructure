6726effa7cc70f1dd781fa3e1bea90674c9c9297
liuyunan <liuyunan.20@gmail.com>
2023-04-27 22:56:06 -0700

    add shortcut, still tle

--- main_problem/DSF/62_unique_paths.py
+++ main_problem/DSF/62_unique_paths.py
@@ -2,15 +2,16 @@ class Solution:
     def uniquePaths_tle(self, m: int, n: int) -> int:
 
         def helper(i, j, path):
-            if i == m - 1 and j == n - 1:
-                return path + 1
-            elif i == m - 1:
-                path = helper(i, j + 1, path)
-            elif j == n - 1:
-                path = helper(i + 1, j, path)
-            else:
-                path = helper(i + 1, j, path) + helper(i, j + 1, path)
-            return path
+            paths = []
 
-        return helper(0, 0, 0)
+            def helper(i, j):
+                if i == m - 1 or j == n - 1:
+                    paths.append(1)
+                    return
+                else:
+                    helper(i + 1, j)
+                    helper(i, j + 1)
+
+            helper(0, 0)
+            return len(paths)
 
