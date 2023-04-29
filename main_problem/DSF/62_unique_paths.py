917de36aee101095503e1f053c8d3890fa562f02
liuyunan <liuyunan.20@gmail.com>
2023-04-28 22:30:39 -0700

    There is a robot on an m x n grid. The robot is initially located at the top-left corner (i.e., grid[0][0]). The robot tries to move to the bottom-right corner (i.e., grid[m - 1][n - 1]). The robot can only move either down or right at any point in time.
    Given the two integers m and n, return the number of possible unique paths that the robot can take to reach the bottom-right corner.
    
    The test cases are generated so that the answer will be less than or equal to 2 * 109.
    
    Example 1:
    
    Input: m = 3, n = 7
    Output: 28
    Example 2:
    
    Input: m = 3, n = 2
    Output: 3
    Explanation: From the top-left corner, there are a total of 3 ways to reach the bottom-right corner:
    1. Right -> Down -> Down
    2. Down -> Down -> Right
    3. Down -> Right -> Down
    
    Constraints:
    
    1 <= m, n <= 100

--- main_problem/DSF/62_unique_paths.py
+++ main_problem/DSF/62_unique_paths.py
@@ -1,17 +1,17 @@
 class Solution:
-    def uniquePaths_tle(self, m: int, n: int) -> int:
+    def uniquePaths(self, m: int, n: int) -> int:
+        memo = {}
 
         def helper(i, j, path):
-            paths = []
+            if (i, j) in memo:
+                return memo[(i, j)]
+            if i == m - 1 or j == n - 1:
+                memo[(i, j)] = path + 1
+                return path + 1
+            else:
+                path = helper(i + 1, j, path) + helper(i, j + 1, path)
+                memo[(i, j)] = path
+                return path
 
-            def helper(i, j):
-                if i == m - 1 or j == n - 1:
-                    paths.append(1)
-                    return
-                else:
-                    helper(i + 1, j)
-                    helper(i, j + 1)
-
-            helper(0, 0)
-            return len(paths)
+        return helper(0, 0, 0)
 
