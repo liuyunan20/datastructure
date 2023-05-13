184d5535de2ee41de354ef29a7af825c5eda4735
liuyunan <liuyunan.20@gmail.com>
2023-05-13 00:13:35 -0700

    get result in O(n)

--- main_problem/datastructure/array/243_shortest_word_distance.py
+++ main_problem/datastructure/array/243_shortest_word_distance.py
@@ -3,11 +3,11 @@ from typing import List
 
 class Solution:
     def shortestDistance(self, wordsDict: List[str], word1: str, word2: str) -> int:
-        words_position = {}
-        for i, word in enumerate(wordsDict):
-            words_position.setdefault(word, []).append(i)
+        words_position = {word1: -1, word2: -1}
         result = len(wordsDict)
-        for i in words_position[word1]:
-            for j in words_position[word2]:
-                result = min(result, abs(i - j))
+        for i, word in enumerate(wordsDict):
+            if word in words_position:
+                words_position[word] = i
+                if words_position[word1] >= 0 and words_position[word2] >= 0:
+                    result = min(result, abs(words_position[word1] - words_position[word2]))
         return result
