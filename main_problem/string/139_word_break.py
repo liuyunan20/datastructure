53b2a46ba877457b78fb8ff0c0da971610b88c7c
liuyunan <liuyunan.20@gmail.com>
2023-05-15 21:04:55 -0700

    tle: "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaabaabaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa" "aa","aaa","aaaa","aaaaa","aaaaaa","aaaaaaa","aaaaaaaa","aaaaaaaaa","aaaaaaaaaa","ba"]

--- main_problem/string/139_word_break.py
+++ main_problem/string/139_word_break.py
@@ -3,15 +3,20 @@ from typing import List
 
 class Solution:
     def wordBreak_tle(self, s: str, wordDict: List[str]) -> bool:
+        letter_set = set(list(''.join(wordDict)))
+        if not set(list(s)).issubset(letter_set):
+            return False
+        wordDict = set(wordDict)
         n = len(s)
+
         def helper(i, j):
             if i == n:
                 return True
             while j < n:
-                print(s[i: j + 1])
                 if s[i: j + 1] and s[i: j + 1] in wordDict and helper(j + 1, j + 1):
                     return True
                 else:
                     j += 1
             return False
+
         return helper(0, 0)
