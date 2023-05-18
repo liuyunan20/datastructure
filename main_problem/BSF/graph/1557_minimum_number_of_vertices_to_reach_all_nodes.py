2490c88c7fd8df28888bf9748e13fedb4a0cb243
liuyunan <liuyunan.20@gmail.com>
2023-05-17 23:09:48 -0700

    faster but not fast enough.

--- main_problem/BSF/graph/1557_minimum_number_of_vertices_to_reach_all_nodes.py
+++ main_problem/BSF/graph/1557_minimum_number_of_vertices_to_reach_all_nodes.py
@@ -3,11 +3,8 @@ from typing import List
 
 class Solution:
     def findSmallestSetOfVertices(self, n: int, edges: List[List[int]]) -> List[int]:
+        result = set(range(n))
         dependent = set()
         for pre, node in edges:
             dependent.add(node)
-        result = set()
-        for i in range(n):
-            if i not in dependent:
-                result.add(i)
-        return result
+        return result - dependent
