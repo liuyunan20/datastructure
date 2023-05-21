0aff56e1ecfab4a3cca918d8e10e6fb1e43f2741
liuyunan <liuyunan.20@gmail.com>
2023-05-21 00:34:20 -0700

    add visited You are given an n x n binary matrix grid where 1 represents land and 0 represents water.
    An island is a 4-directionally connected group of 1's not connected to any other 1's. There are exactly two islands in grid.
    
    You may change 0's to 1's to connect the two islands to form one island.
    
    Return the smallest number of 0's you must flip to connect the two islands.
    
    Example 1:
    
    Input: grid = [[0,1],[1,0]]
    Output: 1
    Example 2:
    
    Input: grid = [[0,1,0],[0,0,0],[0,0,1]]
    Output: 2
    Example 3:
    
    Input: grid = [[1,1,1,1,1],[1,0,0,0,1],[1,0,1,0,1],[1,0,0,0,1],[1,1,1,1,1]]
    Output: 1
    
    Constraints:
    
    n == grid.length == grid[i].length
    2 <= n <= 100
    grid[i][j] is either 0 or 1.
    There are exactly two islands in grid.

--- main_problem/array_and_matrix/934_shortest_bridge.py
+++ main_problem/array_and_matrix/934_shortest_bridge.py
@@ -21,19 +21,43 @@ class Solution:
                     found1 = True
                     break
 
-        result = -1
-        queue = list(land)
-        while queue:
-            result += 1
-            l = len(queue)
-            for _ in range(l):
-                i, j = queue.pop(0)
+        def shortestBridge(self, grid: List[List[int]]) -> int:
+            def find_land(x, y):
                 for h, v in [(-1, 0), (1, 0), (0, -1), (0, 1)]:
-                    a = i + h
-                    b = j + v
-                    if 0 <= a < n and 0 <= b < n:
-                        if grid[a][b] == 0:
-                            queue.append((a, b))
-                        if grid[a][b] == 1 and (a, b) not in land:
-                            return result
+                    a = x + h
+                    b = y + v
+                    if 0 <= a < n and 0 <= b < n and grid[a][b] == 1 and (a, b) not in land:
+                        land.add((a, b))
+                        find_land(a, b)
+
+            land = set()
+            n = len(grid)
+            found1 = False
+            for i in range(n):
+                if found1:
+                    break
+                for j in range(n):
+                    if grid[i][j] == 1:
+                        land.add((i, j))
+                        find_land(i, j)
+                        found1 = True
+                        break
+
+            result = -1
+            queue = list(land)
+            visited = set()
+            while queue:
+                result += 1
+                l = len(queue)
+                for _ in range(l):
+                    i, j = queue.pop(0)
+                    for h, v in [(-1, 0), (1, 0), (0, -1), (0, 1)]:
+                        a = i + h
+                        b = j + v
+                        if 0 <= a < n and 0 <= b < n and (a, b) not in visited:
+                            if grid[a][b] == 0:
+                                queue.append((a, b))
+                            if grid[a][b] == 1 and (a, b) not in land:
+                                return result
+                            visited.add((a, b))
 
