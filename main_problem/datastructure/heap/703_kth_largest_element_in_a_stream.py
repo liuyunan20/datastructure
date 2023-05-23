d8e1eb01241ecf1b99723e68e67679e8bdf30ffd
liuyunan <liuyunan.20@gmail.com>
2023-05-22 20:49:34 -0700

    not use heap, slower

--- main_problem/datastructure/heap/703_kth_largest_element_in_a_stream.py
+++ main_problem/datastructure/heap/703_kth_largest_element_in_a_stream.py
@@ -7,13 +7,12 @@ class KthLargest:
     def __init__(self, k: int, nums: List[int]):
         self.k = k
         self.nums = nums
-        heapq.heapify(self.nums)
 
     def add(self, val: int) -> int:
-        heapq.heappush(self.nums, val)
-        while len(self.nums) > self.k:
-            heapq.heappop(self.nums)
-        return self.nums[0]
+        self.nums.append(val)
+        self.nums.sort()
+        return self.nums[len(self.nums) - self.k]
+
 
 # Your KthLargest object will be instantiated and called as such:
 # obj = KthLargest(k, nums)
