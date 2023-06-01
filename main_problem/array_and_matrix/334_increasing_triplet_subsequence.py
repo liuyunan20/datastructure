812c56b878b83a9d3dae48a02d0bf4368bff2e69
liuyunan <liuyunan.20@gmail.com>
2023-05-31 23:56:18 -0700

    find the smallest possible middle number

--- main_problem/array_and_matrix/334_increasing_triplet_subsequence.py
+++ main_problem/array_and_matrix/334_increasing_triplet_subsequence.py
@@ -2,17 +2,19 @@ from typing import List
 
 
 class Solution:
-    def increasingTriplet_tle(self, nums: List[int]) -> bool:
+    def increasingTriplet(self, nums: List[int]) -> bool:
         n = len(nums)
         if n < 3:
             return False
-        left = [nums[0]]
-        right = [nums[n - 1]]
-        for i in range(1, n):
-            left.append(min(left[-1], nums[i]))
-        for i in range(n - 2, -1, -1):
-            right.insert(0, max(right[0], nums[i]))
-        for i in range(n):
-            if left[i] < nums[i] < right[i]:
+        i = 0
+        left = nums[0]
+        middle = 2147483648
+        while i < n:
+            if nums[i] > middle:
                 return True
+            if left < nums[i]:
+                middle = min(middle, nums[i])
+            else:
+                left = nums[i]
+            i += 1
         return False
