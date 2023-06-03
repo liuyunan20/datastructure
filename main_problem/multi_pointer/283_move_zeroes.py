6e28377250c4ba3051c8da480117dc1bc0df6f58
liuyunan <liuyunan.20@gmail.com>
2023-06-02 20:59:20 -0700

    review this problem again

--- main_problem/multi_pointer/283_move_zeroes.py
+++ main_problem/multi_pointer/283_move_zeroes.py
@@ -6,13 +6,13 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        n = len(nums)
         i = 0
-        count = 0
-        while count < n:
+        j = 0
+        n = len(nums)
+        while j < n:
             if nums[i] == 0:
                 nums.pop(i)
                 nums.append(0)
             else:
                 i += 1
-            count += 1
+            j += 1
