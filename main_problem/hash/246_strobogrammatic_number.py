2aa4ee51aabc8cf41b0ab5fc92dc51e238d8be0e
liuyunan <liuyunan.20@gmail.com>
2023-06-07 07:17:00 -1000

    Given a string num which represents an integer, return true if num is a strobogrammatic number.
    A strobogrammatic number is a number that looks the same when rotated 180 degrees (looked at upside down).
    
    Example 1:
    
    Input: num = "69"
    Output: true
    Example 2:
    
    Input: num = "88"
    Output: true
    Example 3:
    
    Input: num = "962"
    Output: false
    
    Constraints:
    
    1 <= num.length <= 50
    num consists of only digits.
    num does not contain any leading zeros except for zero itself.

---
+++ main_problem/hash/246_strobogrammatic_number.py
@@ -0,0 +1,14 @@
+class Solution:
+    def isStrobogrammatic(self, num: str) -> bool:
+        digits = {'6': '9', '9': '6', '1': '1', '0': '0', '8': '8'}
+        n = len(num)
+        new = ''
+        i = 0
+        while i < n:
+            if num[i] not in digits:
+                return False
+            new += digits[num[i]]
+            i += 1
+        if new[::-1] == num:
+            return True
+        return False
