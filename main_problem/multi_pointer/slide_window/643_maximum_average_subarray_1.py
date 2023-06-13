9264ea6abcd72780a608bcb32b97e26768eaa96b
liuyunan <liuyunan.20@gmail.com>
2023-06-12 23:35:53 -0700

    You are given an integer array nums consisting of n elements, and an integer k.
    Find a contiguous subarray whose length is equal to k that has the maximum average value and return this value. Any answer with a calculation error less than 10-5 will be accepted.
    
    Example 1:
    
    Input: nums = [1,12,-5,-6,50,3], k = 4
    Output: 12.75000
    Explanation: Maximum average is (12 - 5 - 6 + 50) / 4 = 51 / 4 = 12.75
    Example 2:
    
    Input: nums = [5], k = 1
    Output: 5.00000
    
    Constraints:
    
    n == nums.length
    1 <= k <= n <= 105
    -104 <= nums[i] <= 104

--- main_problem/multi_pointer/slide_window/643_maximum_average_subarray_1.py
+++ main_problem/multi_pointer/slide_window/643_maximum_average_subarray_1.py
@@ -1,11 +1,16 @@
+from typing import List
+
+
 class Solution:
-    def findMaxAverage_tle(self, nums: List[int], k: int) -> float:
+    def findMaxAverage(self, nums: List[int], k: int) -> float:
         n = len(nums)
         if k == 1:
             return max(nums)
         i = 0
-        result = -2147483648
-        while i <= n - k:
-            result = max(result, sum(nums[i: i + k]) / k)
+        result = sum(nums[: k]) / k
+        sum_b = result
+        while i < n - k:
+            sum_b += (nums[i + k] - nums[i]) / k
+            result = max(result, sum_b)
             i += 1
         return result
