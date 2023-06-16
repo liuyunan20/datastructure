71d23d6f72348e0140d853b5d8f590d1e9a7e606
liuyunan <liuyunan.20@gmail.com>
2023-06-15 22:32:10 -0700

    Given a binary array nums and an integer k, return the maximum number of consecutive 1's in the array if you can flip at most k 0's.
    Example 1:
    
    Input: nums = [1,1,1,0,0,0,1,1,1,1,0], k = 2
    Output: 6
    Explanation: [1,1,1,0,0,1,1,1,1,1,1]
    Bolded numbers were flipped from 0 to 1. The longest subarray is underlined.
    Example 2:
    
    Input: nums = [0,0,1,1,0,0,1,1,1,0,1,1,0,0,0,1,1,1,1], k = 3
    Output: 10
    Explanation: [0,0,1,1,1,1,1,1,1,1,1,1,0,0,0,1,1,1,1]
    Bolded numbers were flipped from 0 to 1. The longest subarray is underlined.
    
    Constraints:
    
    1 <= nums.length <= 105
    nums[i] is either 0 or 1.
    0 <= k <= nums.length

--- main_problem/multi_pointer/slide_window/1004_max_consecutive_ones_3.py
+++ main_problem/multi_pointer/slide_window/1004_max_consecutive_ones_3.py
@@ -2,15 +2,19 @@ from typing import List
 
 
 class Solution:
-    def longestOnes_tle(self, nums: List[int], k: int) -> int:
+    def longestOnes(self, nums: List[int], k: int) -> int:
         n = len(nums)
         i = 0
-        j = 1
-        result = 0
-        while j <= n:
-            if j - i - sum(nums[i: j]) <= k:
+        j = 0
+        counter = nums[0]
+        result = counter if n > 1 else max(counter, n)
+        while j < n:
+            if j - i + 1 - counter <= k:
+                if j + 1 < n:
+                    counter += nums[j + 1]
                 j += 1
             else:
+                counter -= nums[i]
                 i += 1
-            result = max(result, j - i - 1)
+            result = max(result, j - i)
         return result
