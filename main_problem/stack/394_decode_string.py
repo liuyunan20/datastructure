ac5433639038eb0f00b162af552dec7aa7b6277d
liuyunan <liuyunan.20@gmail.com>
2023-06-25 17:10:35 -0700

    review this problem

--- main_problem/stack/394_decode_string.py
+++ main_problem/stack/394_decode_string.py
@@ -5,29 +5,20 @@ import int as int
 
 class Solution:
     def decodeString(self, s: str) -> str:
-        l = len(s)
+        n = len(s)
         i = 0
         stack = []
-        while i < l:
-            if s[i] == "]":
-                temporary = ""
-                letter = stack.pop()
-                while letter != "[":
-                    temporary = letter + temporary
-                    letter = stack.pop()
-
-                number = ""
-                digit = stack.pop()
-                while digit and digit in string.digits:
-                    number += digit
-                    if stack:
-                        digit = stack.pop()
-                    else:
-                        digit = ""
-                stack.append(digit)
-                number = int(number[::-1])
-                stack.append(temporary * number)
+        while i < n:
+            if s[i] == ']':
+                repeat = ''
+                while stack[-1] != '[':
+                    repeat += stack.pop()
+                stack.pop()
+                num = ''
+                while stack and stack[-1] in string.digits:
+                    num += stack.pop()
+                stack += list(repeat[::-1] * int(num[::-1]))
             else:
                 stack.append(s[i])
             i += 1
-        return "".join(stack)
+        return ''.join(stack)
