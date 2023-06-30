ec89086edda3c6f54e1df990a64c0ffe0d0c0691
liuyunan <liuyunan.20@gmail.com>
2023-06-30 15:29:04 -0700

    review this problem

--- main_problem/linked_list/206_reverse_linked_list.py
+++ main_problem/linked_list/206_reverse_linked_list.py
@@ -7,14 +7,13 @@ class ListNode:
 
 class Solution:
     def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        nodes = []
-        while head:
-            nodes.append(head)
-            head = head.next
-        dummy = ListNode()
-        pre = dummy
-        while nodes:
-            pre.next = nodes.pop()
-            pre = pre.next
-        pre.next = None
-        return dummy.next
+        if not head or not head.next:
+            return head
+        node = head.next
+        head.next = None
+        while node:
+            right = node.next
+            node.next = head
+            head = node
+            node = right
+        return head
