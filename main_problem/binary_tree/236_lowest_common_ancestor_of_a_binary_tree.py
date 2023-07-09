a4f8ef6ca8bd8e1f552d27b4a56fd3a02a8ae8d2
liuyunan <liuyunan.20@gmail.com>
2023-07-08 20:43:45 -0700

    review this problem

--- main_problem/binary_tree/236_lowest_common_ancestor_of_a_binary_tree.py
+++ main_problem/binary_tree/236_lowest_common_ancestor_of_a_binary_tree.py
@@ -6,48 +6,21 @@ class TreeNode:
 
 class Solution:
     def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
-        def find_p(node, target):
+        values = (p.val, q.val)
+        result = []
+
+        def helper(node):
             if not node:
                 return False
-            if node == target:
-                return True
-            return find_p(node.left, target) or find_p(node.right, target)
-
-        child_parent = {}
-        nodes = [root]
-        p_parents = []
-        q_parents = []
-        while nodes:
-            node = nodes.pop(0)
-            if node == p and find_p(node, q):
-                return p
-            if node == q and find_p(node, p):
-                return q
-            if node == p:
-                while p != root:
-                    p_parents.append(child_parent[p])
-                    p = child_parent[p]
-            if node == q:
-                while q != root:
-                    q_parents.append(child_parent[q])
-                    q = child_parent[q]
-            if p == root and q == root:
-                break
-            if node.left:
-                child_parent[node.left] = node
-                nodes.append(node.left)
-            if node.right:
-                child_parent[node.right] = node
-                nodes.append(node.right)
-        result = root
-        print(p_parents)
-        print(q_parents)
-        while True:
-            if not p_parents or not q_parents:
-                return result
-            p = p_parents.pop()
-            q = q_parents.pop()
-            if p != q:
-                return result
+            if node.val in values:
+                mid = True
             else:
-                result = p
+                mid = False
+            left = helper(node.left)
+            right = helper(node.right)
+            if (mid and left) or (mid and right) or (left and right):
+                result.append(node)
+            return mid or left or right
+
+        helper(root)
+        return result[0]
