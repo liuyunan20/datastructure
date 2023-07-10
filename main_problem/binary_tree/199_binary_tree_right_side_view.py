f34d87584df196ab7b00d9082c844a9fefc3bcbc
liuyunan <liuyunan.20@gmail.com>
2023-07-09 23:31:26 -0700

    review this problem

--- main_problem/binary_tree/199_binary_tree_right_side_view.py
+++ main_problem/binary_tree/199_binary_tree_right_side_view.py
@@ -10,13 +10,17 @@ class TreeNode:
 
 class Solution:
     def rightSideView(self, root: Optional[TreeNode]) ->List[int]:
+        if not root:
+            return []
+        nodes = [root]
         right = []
-
-        def helper(node):
-            if not node:
-                return
+        while nodes:
+            n = len(nodes)
+            for _ in range(n):
+                node = nodes.pop(0)
+                if node.left:
+                    nodes.append(node.left)
+                if node.right:
+                    nodes.append(node.right)
             right.append(node.val)
-            helper(node.right)
-
-        helper(root)
         return right
