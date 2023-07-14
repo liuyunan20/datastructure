37f2cca5375d3fe24c2b54e5a92777baecacafbb
liuyunan <liuyunan.20@gmail.com>
2023-07-13 22:38:25 -0700

    Given a root node reference of a BST and a key, delete the node with the given key in the BST. Return the root node reference (possibly updated) of the BST.
    Basically, the deletion can be divided into two stages:
    
    Search for a node to remove.
    If the node is found, delete the node.
    
    Example 1:
    
    Input: root = [5,3,6,2,4,null,7], key = 3
    Output: [5,4,6,2,null,null,7]
    Explanation: Given key to delete is 3. So we find the node with value 3 and delete it.
    One valid answer is [5,4,6,2,null,null,7], shown in the above BST.
    Please notice that another valid answer is [5,2,6,null,4,null,7] and it's also accepted.
    
    Example 2:
    
    Input: root = [5,3,6,2,4,null,7], key = 0
    Output: [5,3,6,2,4,null,7]
    Explanation: The tree does not contain a node with value = 0.
    Example 3:
    
    Input: root = [], key = 0
    Output: []
    
    Constraints:
    
    The number of nodes in the tree is in the range [0, 104].
    -105 <= Node.val <= 105
    Each node has a unique value.
    root is a valid binary search tree.
    -105 <= key <= 105
    
    Follow up: Could you solve it with time complexity O(height of tree)?

--- main_problem/binary_search/450_delete_node_in_a_BST.py
+++ main_problem/binary_search/450_delete_node_in_a_BST.py
@@ -7,40 +7,47 @@ class TreeNode:
 
 class Solution:
     def deleteNode(self, root: Optional[TreeNode], key: int) -> Optional[TreeNode]:
-        def find_left(parent, node):
-            if not node.left:
-                return parent, node
-            return find_left(node, node.left)
+        # class TreeNode:
+        #     def __init__(self, val=0, left=None, right=None):
+        #         self.val = val
+        #         self.left = left
+        #         self.right = right
+        class Solution:
+            def deleteNode(self, root: Optional[TreeNode], key: int) -> Optional[TreeNode]:
+                def find_left(parent, node):
+                    if not node.left:
+                        return parent, node
+                    return find_left(node, node.left)
 
-        def delete(parent, node, key, direct):
-            if not node:
-                return
-            if node.val == key:
-                if not node.right:
-                    if direct == 0:
-                        parent.left = node.left
-                    else:
-                        parent.right = node.right
-                    if parent.right == node:
-                        parent.right = None
-                    return
-                left_p, left = find_left(node, node.right)
-                if left_p != node:
-                    left_p.left = left.right
-                if direct == 0:
-                    parent.left = left
-                else:
-                    parent.right = left
-                left.left = node.left
-                if node.right != left:
-                    left.right = node.right
-                return
-            if key > node.val:
-                delete(node, node.right, key, 1)
-            if key < node.val:
-                delete(node, node.left, key, 0)
+                def delete(parent, node, key, direct):
+                    if not node:
+                        return
+                    if node.val == key:
+                        if not node.right:
+                            if direct == 0:  # 0 means node is parent.left
+                                parent.left = node.left
+                            else:  # 1 means node is parent.right
+                                parent.right = node.left
+                            if parent.right == node:
+                                parent.right = None
+                            return
+                        left_p, left = find_left(node, node.right)
+                        if left_p != node:
+                            left_p.left = left.right
+                        if direct == 0:
+                            parent.left = left
+                        else:
+                            parent.right = left
+                        left.left = node.left
+                        if node.right != left:
+                            left.right = node.right
+                        return
+                    if key > node.val:
+                        delete(node, node.right, key, 1)
+                    if key < node.val:
+                        delete(node, node.left, key, 0)
 
-        pre = TreeNode()
-        pre.left = root
-        delete(pre, root, key, 0)
-        return pre.left
+                pre = TreeNode()
+                pre.left = root
+                delete(pre, root, key, 0)
+                return pre.left
