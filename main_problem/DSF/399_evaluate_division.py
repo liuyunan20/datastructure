e95d6feeb9d76f7c771d7f8a60a1f634bede6c6e
liuyunan <liuyunan.20@gmail.com>
2023-07-19 23:46:11 -0700

    review this problem

--- main_problem/DSF/399_evaluate_division.py
+++ main_problem/DSF/399_evaluate_division.py
@@ -3,33 +3,31 @@ from typing import List
 
 class Solution:
     def calcEquation(self, equations: List[List[str]], values: List[float], queries: List[List[str]]) -> List[float]:
-        def dfs(x1, x2, val, visited):
-            if x1 == x2:
-                return val
-            value = -1
-            visited.add(x1)
-            for x3, v in dic[x1]:
-                if x3 not in visited:
-                    value = dfs(x3, x2, val * v, visited)
-                    if value > 0:
-                        return value
-            return value
+        def get_value(q1, q2, visited, v):
+            if q1 == q2:
+                return (True, v)
+            for letter, val in maps[q1]:
+                if letter in visited:
+                    continue
+                visited.append(letter)
+                find, value = get_value(letter, q2, list(visited), v * val)
+                if find:
+                    return (find, value)
+            return (False, -1)
 
-        dic = {}
-        for i in range(len(equations)):
-            dic.setdefault(equations[i][0], set()).add((equations[i][1], values[i]))
-            dic.setdefault(equations[i][1], set()).add((equations[i][0], 1 / values[i]))
-
+        n = len(equations)
+        maps = {}
+        for i in range(n):
+            maps.setdefault(equations[i][0], []).append((equations[i][1], values[i]))
+            maps.setdefault(equations[i][1], []).append((equations[i][0], 1 / values[i]))
         result = []
         for q1, q2 in queries:
-            value = 1
-            if q1 in dic and q1 == q2:
-                result.append(1.0)
-                continue
-            if q1 not in dic or q2 not in dic:
-                result.append(-1.0)
-                continue
-            value = dfs(q1, q2, 1.0, set())
-            result.append(value)
-
+            if q1 not in maps or q2 not in maps:
+                result.append(-1)
+            else:
+                find, value = get_value(q1, q2, [], 1)
+                if find:
+                    result.append(value)
+                else:
+                    result.append(-1)
         return result
