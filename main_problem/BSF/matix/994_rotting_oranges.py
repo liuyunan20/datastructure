6a987f0effe3c4b41e158004b927d83dfcf3650a
liuyunan <liuyunan.20@gmail.com>
2023-07-21 21:18:54 -0700

    review this problem

--- main_problem/BSF/matix/994_rotting_oranges.py
+++ main_problem/BSF/matix/994_rotting_oranges.py
@@ -3,34 +3,35 @@ from typing import List
 
 class Solution:
     def orangesRotting(self, grid: List[List[int]]) -> int:
-        m, n = len(grid), len(grid[0])
-        queue = []
+        m = len(grid)
+        n = len(grid[0])
         visited = set()
-        fresh = False
+        queue = []
+        step = 0
         for i in range(m):
             for j in range(n):
                 if grid[i][j] == 2:
                     queue.append((i, j))
                     visited.add((i, j))
+                    step = -1
                 if grid[i][j] == 1:
-                    fresh = True
-        if not queue and not fresh:
-            return 0
-        minute = -1
+                    step = -1
+
         while queue:
-            minute += 1
             l = len(queue)
+            step += 1
             for _ in range(l):
                 i, j = queue.pop(0)
-                for d in [(1, 0), (-1, 0), (0, 1), (0, -1)]:
-                    r = i + d[0]
-                    c = j + d[1]
-                    if 0 <= r < m and 0 <= c < n and (r, c) not in visited and grid[r][c] == 1:
-                        grid[r][c] = 2
-                        queue.append((r, c))
-                        visited.add((r, c))
+                for h, v in [(0, 1), (0, -1), (1, 0), (-1, 0)]:
+                    a = i + h
+                    b = j + v
+                    if 0 <= a < m and 0 <= b < n and grid[a][b] == 1:
+                        grid[a][b] = 2
+                        queue.append((a, b))
+                        visited.add((a, b))
+
         for i in range(m):
             for j in range(n):
                 if grid[i][j] == 1:
                     return -1
-        return minute
+        return step
