64bf3e2fbe704502c76f739658a5544130d560d1
liuyunan <liuyunan.20@gmail.com>
2023-08-02 23:37:05 -0700

    RecursionError: maximum recursion depth exceeded in comparison with x = 0.00001 n = 2147483647

--- main_problem/50_pow(x, n).py
+++ main_problem/50_pow(x, n).py
@@ -1,14 +1,10 @@
 class Solution:
     def myPow_tle(self, x: float, n: int) -> float:
-        res = 1
         if x == 0:
             return x
         if n == 0:
             return 1
         if n > 0:
-            for _ in range(n):
-                res *= x
+            return self.myPow(x, n - 1) * x
         if n < 0:
-            for _ in range(-n):
-                res /= x
-        return res
+            return self.myPow(x, n + 1) / x
