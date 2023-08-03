5dcbb7d132af502fb077346c9f1b52bb86608d06
liuyunan <liuyunan.20@gmail.com>
2023-08-02 23:52:53 -0700

    Implement pow(x, n), which calculates x raised to the power n (i.e., xn).
    Example 1:
    
    Input: x = 2.00000, n = 10
    Output: 1024.00000
    Example 2:
    
    Input: x = 2.10000, n = 3
    Output: 9.26100
    Example 3:
    
    Input: x = 2.00000, n = -2
    Output: 0.25000
    Explanation: 2-2 = 1/22 = 1/4 = 0.25
    
    Constraints:
    
    -100.0 < x < 100.0
    -231 <= n <= 231-1
    n is an integer.
    Either x is not zero or n > 0.
    -104 <= xn <= 104

--- main_problem/50_pow(x, n).py
+++ main_problem/50_pow(x, n).py
@@ -4,7 +4,10 @@ class Solution:
             return x
         if n == 0:
             return 1
-        if n > 0:
-            return self.myPow(x, n - 1) * x
         if n < 0:
-            return self.myPow(x, n + 1) / x
+            n = -n
+            x = 1 / x
+        if n % 2 == 0:
+            return self.myPow(x * x, n // 2)
+        else:
+            return self.myPow(x * x, n // 2) * x
