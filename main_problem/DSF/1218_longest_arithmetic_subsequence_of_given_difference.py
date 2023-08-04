37f202e151decaeeaae9403ca91943dbec0b21ee
liuyunan <liuyunan.20@gmail.com>
2023-08-03 22:26:16 -0700

    Given an integer array arr and an integer difference, return the length of the longest subsequence in arr which is an arithmetic sequence such that the difference between adjacent elements in the subsequence equals difference.
    A subsequence is a sequence that can be derived from arr by deleting some or no elements without changing the order of the remaining elements.
    
    Example 1:
    
    Input: arr = [1,2,3,4], difference = 1
    Output: 4
    Explanation: The longest arithmetic subsequence is [1,2,3,4].
    Example 2:
    
    Input: arr = [1,3,5,7], difference = 1
    Output: 1
    Explanation: The longest arithmetic subsequence is any single element.
    Example 3:
    
    Input: arr = [1,5,7,8,5,3,4,2,1], difference = -2
    Output: 4
    Explanation: The longest arithmetic subsequence is [7,5,3,1].
    
    Constraints:
    
    1 <= arr.length <= 105
    -104 <= arr[i], difference <= 104

--- main_problem/DSF/1218_longest_arithmetic_subsequence_of_given_difference.py
+++ main_problem/DSF/1218_longest_arithmetic_subsequence_of_given_difference.py
@@ -5,19 +5,26 @@ class Solution:
             nums.setdefault(num, []).append(i)
         sub_lengths = {}
 
-        def sub_length(i, length):
+        sub_lengths = {}
+        def sub_length(i):
             num = arr[i]
             target = num + difference
             if target not in nums or i >= nums[target][-1]:
-                sub_lengths[i] = length
-                return length
-            result = length
+                sub_lengths[i] = 1
+                return 1
+            result = 1
             for j in nums[target]:
                 if j > i:
-                    sub_lengths[j] = sub_length(j, length + 1)
+                    if j not in sub_lengths:
+                        sub_lengths[j] = sub_length(j)
                     result = max(result, sub_lengths[j])
-            sub_lengths[i] = result
-            return result
+            sub_lengths[i] = result + 1
+            return sub_lengths[i]
+        res = 1
+        for x in range(len(arr)):
+            if x not in sub_lengths:
+                res = max(sub_length(x), res)
+        return res
 
         res = 1
         for x in range(len(arr)):
