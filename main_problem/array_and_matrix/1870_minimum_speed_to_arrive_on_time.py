1e80d6665fefc71b7d05dc78343bd704405659c1
liuyunan <liuyunan.20@gmail.com>
2023-08-12 22:42:58 -0700

    You are given a floating-point number hour, representing the amount of time you have to reach the office. To commute to the office, you must take n trains in sequential order. You are also given an integer array dist of length n, where dist[i] describes the distance (in kilometers) of the ith train ride.
    Each train can only depart at an integer hour, so you may need to wait in between each train ride.
    
    For example, if the 1st train ride takes 1.5 hours, you must wait for an additional 0.5 hours before you can depart on the 2nd train ride at the 2 hour mark.
    Return the minimum positive integer speed (in kilometers per hour) that all the trains must travel at for you to reach the office on time, or -1 if it is impossible to be on time.
    
    Tests are generated such that the answer will not exceed 107 and hour will have at most two digits after the decimal point.
    
    Example 1:
    
    Input: dist = [1,3,2], hour = 6
    Output: 1
    Explanation: At speed 1:
    - The first train ride takes 1/1 = 1 hour.
    - Since we are already at an integer hour, we depart immediately at the 1 hour mark. The second train takes 3/1 = 3 hours.
    - Since we are already at an integer hour, we depart immediately at the 4 hour mark. The third train takes 2/1 = 2 hours.
    - You will arrive at exactly the 6 hour mark.
    Example 2:
    
    Input: dist = [1,3,2], hour = 2.7
    Output: 3
    Explanation: At speed 3:
    - The first train ride takes 1/3 = 0.33333 hours.
    - Since we are not at an integer hour, we wait until the 1 hour mark to depart. The second train ride takes 3/3 = 1 hour.
    - Since we are already at an integer hour, we depart immediately at the 2 hour mark. The third train takes 2/3 = 0.66667 hours.
    - You will arrive at the 2.66667 hour mark.
    Example 3:
    
    Input: dist = [1,3,2], hour = 1.9
    Output: -1
    Explanation: It is impossible because the earliest the third train can depart is at the 2 hour mark.
    
    Constraints:
    
    n == dist.length
    1 <= n <= 105
    1 <= dist[i] <= 105
    1 <= hour <= 109
    There will be at most two digits after the decimal point in hour.

--- main_problem/array_and_matrix/1870_minimum_speed_to_arrive_on_time.py
+++ main_problem/array_and_matrix/1870_minimum_speed_to_arrive_on_time.py
@@ -1,13 +1,42 @@
+from typing import List
+import math
+
+
 class Solution:
     def minSpeedOnTime(self, dist: List[int], hour: float) -> int:
-        if len(dist) > hour + 1:
-            return -1
-        result = 1
-        while True:
+        def get_time(speed):
             time = 0
             for i in range(len(dist) - 1):
-                time += dist[i] // result if dist[i] % result == 0 else dist[i] // result + 1
-            time += dist[-1] / result
-            if time <= hour:
-                return result
-            result += 1
+                r = dist[i] % speed
+                d = dist[i] // speed
+                time += d if r == 0 else d + 1
+            time += dist[-1] / speed
+            return time
+
+        if len(dist) >= hour + 1:
+            return -1
+        spd = math.floor(sum(dist) / hour)
+        spd = spd if spd != 0 else 1
+        pre = spd
+        while True:
+            time = get_time(spd)
+            if time == hour:
+                return spd
+            elif time < hour:
+                while pre < spd:
+                    cur_spd = math.ceil((pre + spd) / 2)
+                    if cur_spd == spd:
+                        return spd
+                    time = get_time(cur_spd)
+                    if time == hour:
+                        return cur_spd
+                    elif time < hour:
+                        spd = cur_spd
+                    else:
+                        pre = cur_spd
+                return pre
+            else:
+                pre = spd
+                spd *= 2
+
+
