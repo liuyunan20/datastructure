04ff1775ab0511eae63063fa52f3a83f6ca221b2
liuyunan <liuyunan.20@gmail.com>
2023-08-13 20:47:00 -0700

    Given an integer array nums, return the third distinct maximum number in this array. If the third maximum does not exist, return the maximum number.
    Example 1:
    
    Input: nums = [3,2,1]
    Output: 1
    Explanation:
    The first distinct maximum is 3.
    The second distinct maximum is 2.
    The third distinct maximum is 1.
    Example 2:
    
    Input: nums = [1,2]
    Output: 2
    Explanation:
    The first distinct maximum is 2.
    The second distinct maximum is 1.
    The third distinct maximum does not exist, so the maximum (2) is returned instead.
    Example 3:
    
    Input: nums = [2,2,3,1]
    Output: 1
    Explanation:
    The first distinct maximum is 3.
    The second distinct maximum is 2 (both 2's are counted together since they have the same value).
    The third distinct maximum is 1.
    
    Constraints:
    
    1 <= nums.length <= 104
    -231 <= nums[i] <= 231 - 1
    
    Follow up: Can you find an O(n) solution?

---
+++ main_problem/array_and_matrix/414_third_maximum_number.py
@@ -0,0 +1,6 @@
+class Solution:
+    def thirdMax(self, nums: List[int]) -> int:
+        nums = set(nums)
+        nums = list(nums)
+        nums.sort()
+        return nums[-3] if len(nums) >= 3 else nums[-1]
