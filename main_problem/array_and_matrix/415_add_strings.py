db50b391588ff243da5fa200fcb27a05c17465af
liuyunan <liuyunan.20@gmail.com>
2023-08-19 22:13:27 -0700

    Given two non-negative integers, num1 and num2 represented as string, return the sum of num1 and num2 as a string.
    You must solve the problem without using any built-in library for handling large integers (such as BigInteger). You must also not convert the inputs to integers directly.
    
    Example 1:
    
    Input: num1 = "11", num2 = "123"
    Output: "134"
    Example 2:
    
    Input: num1 = "456", num2 = "77"
    Output: "533"
    Example 3:
    
    Input: num1 = "0", num2 = "0"
    Output: "0"
    
    Constraints:
    
    1 <= num1.length, num2.length <= 104
    num1 and num2 consist of only digits.
    num1 and num2 don't have any leading zeros except for the zero itself.

--- main_problem/array_and_matrix/415_add_strings.py
+++ main_problem/array_and_matrix/415_add_strings.py
@@ -1,5 +1,16 @@
 class Solution:
     def addStrings(self, num1: str, num2: str) -> str:
-        num1 = int(num1)
-        num2 = int(num2)
-        return str(num1 + num2)
+        def get_num(num):
+            num = num[::-1]
+            result = int(num[0])
+            for i in range(1, len(num)):
+                result += 10 ** i * int(num[i])
+            return result
+        n1 = get_num(num1)
+        n2 = get_num(num2)
+        res = n1 + n2
+        result = ""
+        while res != 0:
+            result += str(res % 10)
+            res = res // 10
+        return result[::-1] if result else "0"
