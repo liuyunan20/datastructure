fc9095d9120450513df051d67ce0a5a7ed95a821
liuyunan <liuyunan.20@gmail.com>
2023-09-07 23:58:21 -0700

    still tle

--- main_problem/array_and_matrix/134_gas_station.py
+++ main_problem/array_and_matrix/134_gas_station.py
@@ -1,14 +1,12 @@
-from typing import List
-
-
 class Solution:
     def canCompleteCircuit(self, gas: List[int], cost: List[int]) -> int:
         def circle(idx):
-            tank = 0
             start = idx - 1 if idx > 0 else len(gas) - 1
+            tank = 0
             while idx != start:
                 tank += gas[idx] - cost[idx]
                 if tank < 0:
+                    end = idx
                     return False
                 else:
                     idx = 0 if idx == len(gas) - 1 else idx + 1
@@ -18,7 +16,12 @@ class Solution:
             return -1
         n = len(gas)
         i = 0
-        for i in range(n):
+        end = -1
+        while i < n:
+            if end >= 0:
+                i = end
+            else:
+                i += 1
             if circle(i):
                 return i
         return -1
