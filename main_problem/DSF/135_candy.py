c5cfa121aeac6c6ea8fdac47dd5c5ecc7f0ef231
liuyunan <liuyunan.20@gmail.com>
2023-09-10 22:58:51 -0700

    tle

--- main_problem/DSF/135_candy.py
+++ main_problem/DSF/135_candy.py
@@ -8,19 +8,18 @@ class Solution:
         for i, r in enumerate(ratings):
             low_position.append((i, r))
         low_position.sort(key=lambda x:x[1])
-        while low_position:
+        while low_position:            
             p, r = low_position.pop(0)
             visited.add(p)
             left = p - 1
-            while left >= 0 and ratings[left] > ratings[left + 1]:
-                result[left] = result[left + 1] + 1
+            while left >= 0 and ratings[left] > ratings[left + 1]: 
+                result[left] = max(result[left + 1] + 1, result[left])
                 left -= 1
             # if left >= 0 and left not in visited:
-            #     low_position.append((left, ratings[left]))
+            #     low_position.append((left, ratings[left]))             
             right = p + 1
             while right < n and ratings[right] > ratings[right - 1]:
-                result[right] = result[right - 1] + 1
-                rate = ratings[right]
+                result[right] = max(result[right - 1] + 1, result[right])
                 right += 1
             # if right < n and right not in visited:
             #     low_position.append((right, ratings[right]))
