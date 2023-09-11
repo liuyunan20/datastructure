ee674a99f61d52ee28cd60dd22797ef43e1fc4dc
liuyunan <liuyunan.20@gmail.com>
2023-09-10 23:02:26 -0700

    There are n children standing in a line. Each child is assigned a rating value given in the integer array ratings.
    You are giving candies to these children subjected to the following requirements:
    
    Each child must have at least one candy.
    Children with a higher rating get more candies than their neighbors.
    Return the minimum number of candies you need to have to distribute the candies to the children.
    
    Example 1:
    
    Input: ratings = [1,0,2]
    Output: 5
    Explanation: You can allocate to the first, second and third child with 2, 1, 2 candies respectively.
    Example 2:
    
    Input: ratings = [1,2,2]
    Output: 4
    Explanation: You can allocate to the first, second and third child with 1, 2, 1 candies respectively.
    The third child gets 1 candy because it satisfies the above two conditions.
    
    Constraints:
    
    n == ratings.length
    1 <= n <= 2 * 104
    0 <= ratings[i] <= 2 * 104

--- main_problem/DSF/135_candy.py
+++ main_problem/DSF/135_candy.py
@@ -6,23 +6,26 @@ class Solution:
         low_position = []
         visited = set()
         for i, r in enumerate(ratings):
-            low_position.append((i, r))
-        low_position.sort(key=lambda x:x[1])
-        while low_position:            
-            p, r = low_position.pop(0)
-            visited.add(p)
-            left = p - 1
-            while left >= 0 and ratings[left] > ratings[left + 1]: 
-                result[left] = max(result[left + 1] + 1, result[left])
-                left -= 1
-            # if left >= 0 and left not in visited:
-            #     low_position.append((left, ratings[left]))             
-            right = p + 1
-            while right < n and ratings[right] > ratings[right - 1]:
-                result[right] = max(result[right - 1] + 1, result[right])
-                right += 1
-            # if right < n and right not in visited:
-            #     low_position.append((right, ratings[right]))
-        print(result)
-        print(n, len(visited))
+            if r == lowest:
+                low_position.append((i, r))
+
+        while low_position:
+            low_position.sort(key=lambda x: x[1])
+            l = len(low_position)
+            for _ in range(l):
+                p, r = low_position.pop(0)
+                visited.add(p)
+                left = p - 1
+                while left >= 0 and ratings[left] > ratings[left + 1]:
+                    result[left] = max(result[left + 1] + 1, result[left])
+                    left -= 1
+                if left >= 0 and left not in visited:
+                    low_position.append((left, ratings[left]))
+                right = p + 1
+                while right < n and ratings[right] > ratings[right - 1]:
+                    result[right] = max(result[right - 1] + 1, result[right])
+                    right += 1
+                if right < n and right not in visited:
+                    low_position.append((right, ratings[right]))
+
         return sum(result)
