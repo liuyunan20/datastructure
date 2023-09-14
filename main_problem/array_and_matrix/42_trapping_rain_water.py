21623d2693acba6192e7e6f8a1be23ee45f6d7f0
liuyunan <liuyunan.20@gmail.com>
2023-09-13 21:13:39 -0700

    wrong with [5,5,1,7,1,1,5,2,7,6]

--- main_problem/array_and_matrix/42_trapping_rain_water.py
+++ main_problem/array_and_matrix/42_trapping_rain_water.py
@@ -2,22 +2,40 @@ class Solution:
     def trap(self, height: List[int]) -> int:
         i = 0
         n = len(height)
-
-        result = 0
-        low = []
+        water = 0
+        high_idx = []
+        high_level = []
         while i < n:
-            if i - 1 >= 0 and i + 1 < n and height[i - 1] < height[i] < height[i + 1]:
-                low.append(height[i])
+            if i == 0 and i + 1 < n and height[i] >= height[i + 1]:
+                high_idx.append(i)
+                high_level.append(height[i])
+            if i - 1 >= 0 and i + 1 < n and height[i - 1] <= height[i] and height[i] >= height[i + 1]:
+                high_idx.append(i)
+                high_level.append(height[i])
+            if i == n - 1 and i - 1 >= 0 and height[i - 1] <= height[i]:
+                high_idx.append(i)
+                high_level.append(height[i])
             i += 1
-        while low:
-            water = 0
-            i = low.pop()
-            left = i - 1
-            while left - 1 >= 0 and height[left - 1] > height[left]:
-                left -= 1
-            right = i + 1
-            while right + 1 < n and height[right + 1] > height[right]:
-                right += 1
-            top = max(height[left], height[right])
-            for j in range(left + 1, right):
-                water += height[j] - height[i]
+        if len(high_idx) < 2:
+            return 0
+        tops = [high_idx.pop(0)]
+        left_level = high_level.pop(0)
+        next_top = max(high_level)
+        i = 0
+        while next_top and i < len(high_level):
+            if high_level[i] == next_top:
+                tops.append(high_idx[i])
+            elif high_level[i] >= left_level:
+                tops.append(high_idx[i])
+                left_level = high_level[i]
+            next_top = max(high_level[i + 1:]) if high_level[i + 1:] else 0
+            i += 1
+        left = tops.pop(0)
+        water = 0
+        while tops:
+            right = tops.pop(0)
+            level = min(height[left], height[right])
+            for j in range(left, right):
+                water += max(level - height[j], 0)
+            left = right
+        return water
