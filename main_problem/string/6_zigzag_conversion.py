a75108fc05159c0ff2a54ff4f0fb774b850c018a
liuyunan <liuyunan.20@gmail.com>
2023-09-19 21:27:15 -0700

    review this problem

--- main_problem/string/6_zigzag_conversion.py
+++ main_problem/string/6_zigzag_conversion.py
@@ -1,15 +1,31 @@
 class Solution:
-    def convert(self, s: str, numRows: int) -> str:
-        if numRows == 1:
-            return s
-        result = []
-        for j in range(numRows):
-            i = j
-            while i < len(s):
-                result.append(s[i])
-                if j not in [0, numRows - 1]:
-                    m = i + numRows - (j + 1) + numRows - 2 - (j - 1)
-                    if m < len(s):
-                        result.append(s[m])
-                i = i + 2 * numRows - 2
-        return ''.join(result)
+    class Solution:
+        def convert(self, s: str, numRows: int) -> str:
+            result = [[] for _ in range(numRows)]
+            i = 0
+            n = len(s)
+            middle = []
+            col = 0
+            while i < n:
+                for j in range(numRows):
+                    if i < n:
+                        result[j].append(s[i])
+                        i += 1
+                    else:
+                        break
+                col += 1
+                for _ in range(1, numRows - 1):
+                    for j in range(numRows):
+                        if i >= n:
+                            break
+                        elif numRows - 1 - j == col % (numRows - 1):
+                            result[j].append(s[i])
+                            i += 1
+                        else:
+                            result[j].append('')
+                    col += 1
+
+            for i in range(numRows):
+                result[i] = ''.join(result[i])
+            return ''.join(result)
+
