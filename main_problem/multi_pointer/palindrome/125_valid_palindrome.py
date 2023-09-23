84b7541cd4583af24d4ddee94523d74817a0308f
liuyunan <liuyunan.20@gmail.com>
2023-09-22 23:48:33 -0700

    review this problem

--- main_problem/multi_pointer/palindrome/125_valid_palindrome.py
+++ main_problem/multi_pointer/palindrome/125_valid_palindrome.py
@@ -1,16 +1,14 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        i = 0
-        j = len(s) - 1
+        i, j = 0, len(s) - 1
         while i < j:
             if not s[i].isalnum():
                 i += 1
-                continue
-            if not s[j].isalnum():
+            elif not s[j].isalnum():
                 j -= 1
-                continue
-            if s[i].lower() != s[j].lower():
+            elif s[i].lower() != s[j].lower():
                 return False
-            i += 1
-            j -= 1
+            else:
+                i += 1
+                j -= 1
         return True
