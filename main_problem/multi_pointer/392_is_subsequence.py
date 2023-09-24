f19c0dee7c5bc0e0d629a670b66fa5b042549aaa
liuyunan <liuyunan.20@gmail.com>
2023-09-23 21:57:46 -0700

    review this problem

--- main_problem/multi_pointer/392_is_subsequence.py
+++ main_problem/multi_pointer/392_is_subsequence.py
@@ -1,15 +1,12 @@
 class Solution:
     def isSubsequence(self, s: str, t: str) -> bool:
-        if not s:
-            return True
         i = 0
         j = 0
-        ns = len(s)
-        nt = len(t)
-        while j < nt:
+        ns, nt = len(s), len(t)
+        while i < ns and j < nt:
             if s[i] == t[j]:
                 i += 1
-            j += 1
-            if i == ns:
-                return True
-        return False
+                j += 1
+            else:
+                j += 1
+        return False if i < ns and j == nt else True
