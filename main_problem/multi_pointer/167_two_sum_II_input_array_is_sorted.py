ac05a72aee34a834ed81eac31eb7370b9072b535
liuyunan <liuyunan.20@gmail.com>
2023-09-24 22:49:23 -0700

    Given a 1-indexed array of integers numbers that is already sorted in non-decreasing order, find two numbers such that they add up to a specific target number. Let these two numbers be numbers[index1] and numbers[index2] where 1 <= index1 < index2 < numbers.length.
    Return the indices of the two numbers, index1 and index2, added by one as an integer array [index1, index2] of length 2.
    
    The tests are generated such that there is exactly one solution. You may not use the same element twice.
    
    Your solution must use only constant extra space.
    
    Example 1:
    
    Input: numbers = [2,7,11,15], target = 9
    Output: [1,2]
    Explanation: The sum of 2 and 7 is 9. Therefore, index1 = 1, index2 = 2. We return [1, 2].
    Example 2:
    
    Input: numbers = [2,3,4], target = 6
    Output: [1,3]
    Explanation: The sum of 2 and 4 is 6. Therefore index1 = 1, index2 = 3. We return [1, 3].
    Example 3:
    
    Input: numbers = [-1,0], target = -1
    Output: [1,2]
    Explanation: The sum of -1 and 0 is -1. Therefore index1 = 1, index2 = 2. We return [1, 2].
    
    Constraints:
    
    2 <= numbers.length <= 3 * 104
    -1000 <= numbers[i] <= 1000
    numbers is sorted in non-decreasing order.
    -1000 <= target <= 1000
    The tests are generated such that there is exactly one solution.

--- main_problem/multi_pointer/167_two_sum_II_input_array_is_sorted.py
+++ main_problem/multi_pointer/167_two_sum_II_input_array_is_sorted.py
@@ -1,12 +1,8 @@
 class Solution:
     def twoSum(self, numbers: List[int], target: int) -> List[int]:
-        i = 0
-        n = len(numbers)
-        while i < n - 1:
-            j = i + 1
-            diff = target - numbers[i]
-            while j < n and diff >= numbers[j]:
-                if diff == numbers[j]:
-                    return [i + 1, j + 1]
-                j += 1
-            i += 1
+        freq = {}
+        for i, num in enumerate(numbers):
+            diff = target - num
+            if diff in freq:
+                return [freq[diff][0] + 1, i + 1]
+            freq.setdefault(num, []).append(i)
