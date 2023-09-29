e44462d33e3c081d2185a7f1217545c603dba63e
liuyunan <liuyunan.20@gmail.com>
2023-09-28 23:27:56 -0700

    review this problem

--- main_problem/multi_pointer/slide_window/209_minimum_size_subarray_sum.py
+++ main_problem/multi_pointer/slide_window/209_minimum_size_subarray_sum.py
@@ -2,36 +2,19 @@ from typing import List
 
 
 class Solution:
-    def minSubArrayLen_tle(self, target: int, nums: List[int]) -> int:
-        if sum(nums) < target:
-            return 0
-        i = 0
-        j = 0
-        result = len(nums)
-        while j < len(nums):
-            if sum(nums[i: j + 1]) < target:
-                j += 1
-            else:
-                result = min(result, j - i + 1)
-                i += 1
-        return result
-
     def minSubArrayLen(self, target: int, nums: List[int]) -> int:
-        if sum(nums) < target:
-            return 0
-        i = 0
-        j = 0
+        result = [0]
+        i = j = s = 0
         n = len(nums)
-        result = n
-        sub_sum = 0
-        while j <= n:
-            if sub_sum < target and j < n:
-                sub_sum += nums[j]
+        while j < n:
+            s += nums[j]
+            if s < target:
                 j += 1
-            elif sub_sum >= target:
-                result = min(result, j - i)
-                sub_sum -= nums[i]
-                i += 1
             else:
-                break
-        return result
+                while i < n and s - nums[i] >= target:
+                    s -= nums[i]
+                    i += 1
+                result.append(j - i + 1)
+                j += 1
+        result.sort()
+        return result[1] if len(result) > 1 else 0
