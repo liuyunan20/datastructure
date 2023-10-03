7cc8fa800e2ebf4f205e16b4b8b7dcc7a2424b82
liuyunan <liuyunan.20@gmail.com>
2023-10-02 23:16:40 -0700

    review this problem

--- main_problem/array_and_matrix/54_spiral_matrix.py
+++ main_problem/array_and_matrix/54_spiral_matrix.py
@@ -4,27 +4,16 @@ from _ast import List
 class Solution:
     def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
         result = []
-        row = len(matrix)
-        col = len(matrix[0])
-        while len(result) <= row * col:
-            result += matrix[0]
-            matrix = matrix[1:]
-            if len(result) == row * col:
-                return result
-            m = len(matrix)
-            n = len(matrix[0])
-            for i in range(m):
-                result.append(matrix[i][n - 1])
-                matrix[i] = matrix[i][:n - 1]
-            if len(result) == row * col:
-                return result
-            result += matrix[m - 1][::-1]
-            matrix = matrix[:m - 1]
-            if len(result) == row * col:
-                return result
-            m = len(matrix)
-            n = len(matrix[0])
-            for i in range(m - 1, -1, -1):
-                result.append(matrix[i][0])
-                matrix[i] = matrix[i][1:]
+        while matrix:
+            result += matrix.pop(0)
+            if matrix:
+                for row in matrix:
+                    if row:
+                        result.append(row.pop())
+            if matrix:
+                result += matrix.pop()[::-1]
+            if matrix:
+                for row in matrix[::-1]:
+                    if row:
+                        result.append(row.pop(0))
         return result
