3e6028136da2edbd2b362100d1b46ec0bb0fdeb9
liuyunan <liuyunan.20@gmail.com>
2023-10-03 23:10:51 -0700

    review this problem

--- main_problem/array_and_matrix/48_rotate_image.py
+++ main_problem/array_and_matrix/48_rotate_image.py
@@ -6,31 +6,8 @@ class Solution:
         """
         Do not return anything, modify matrix in-place instead.
         """
+        copy = [list(row) for row in matrix]
         n = len(matrix)
-        j = 0
-        while n - 2 * j > 1:
-            for i in range(n - 2 * j - 1):
-                converter1 = matrix[j][j + i]
-                matrix[j][j + i] = matrix[n - 1 - j - i][j]
-                converter2 = matrix[j + i][n - 1 - j]
-                matrix[j + i][n - 1 - j] = converter1
-                converter1 = matrix[n - 1 - j][n - 1 - j - i]
-                matrix[n - 1 - j][n - 1 - j - i] = converter2
-                matrix[n - 1 - j - i][j] = converter1
-                print(matrix)
-            j += 1
-
-    def rotate_2(self, matrix: List[List[int]]) -> None:
-        """
-        Do not return anything, modify matrix in-place instead.
-        """
-        n = len(matrix)
-        j = 0
-        while n - 2 * j > 1:
-            for i in range(n - 2 * j - 1):
-                converter = matrix[j][j + i]
-                matrix[j][j + i] = matrix[n - 1 - j - i][j]
-                matrix[n - 1 - j - i][j] = matrix[n - 1 - j][n - 1 - j - i]
-                matrix[n - 1 - j][n - 1 - j - i] = matrix[j + i][n - 1 - j]
-                matrix[j + i][n - 1 - j] = converter
-            j += 1
+        for i in range(n):
+            for j in range(n):
+                matrix[i][j] = copy[n - 1 - j][i]
