3c1ec37c4894c99b76506eabba5a7b4bdd344dce
liuyunan <liuyunan.20@gmail.com>
2023-10-16 22:30:25 -0700

    Given an array of intervals where intervals[i] = [starti, endi], merge all overlapping intervals, and return an array of the non-overlapping intervals that cover all the intervals in the input.
    Example 1:
    
    Input: intervals = [[1,3],[2,6],[8,10],[15,18]]
    Output: [[1,6],[8,10],[15,18]]
    Explanation: Since intervals [1,3] and [2,6] overlap, merge them into [1,6].
    Example 2:
    
    Input: intervals = [[1,4],[4,5]]
    Output: [[1,5]]
    Explanation: Intervals [1,4] and [4,5] are considered overlapping.
    
    Constraints:
    
    1 <= intervals.length <= 104
    intervals[i].length == 2
    0 <= starti <= endi <= 104

--- main_problem/multi_pointer/interval/56_merge_intervals.py
+++ main_problem/multi_pointer/interval/56_merge_intervals.py
@@ -1,22 +1,13 @@
 class Solution:
     def merge(self, intervals: List[List[int]]) -> List[List[int]]:
-        intervals.sort(key=lambda x:x[0])
+        intervals.sort(key=lambda x: x[0])
         n = len(intervals)
-        if n == 1:
-            return intervals
-        i = 1
         result = []
-        start = intervals[0][0]
-        end = intervals[0][1]
-        while i < n:
-            if end < intervals[i][0]:
+        start, end = intervals[0]
+        for i in range(n - 1):
+            end = max(intervals[i][1], end)
+            if end < intervals[i + 1][0]:
                 result.append([start, end])
-                start = intervals[i][0]
-                end = intervals[i][1]
-            else:
-                if end < intervals[i][1]:
-                    end = intervals[i][1]
-            if i == n - 1:
-                result.append([start, end])
-            i += 1
+                start = intervals[i + 1][0]
+        result.append([start, max(end, intervals[n - 1][1])])
         return result
