d90d066c8fdc6a10c72aa3338cd54d89e9eb703f
liuyunan <liuyunan.20@gmail.com>
2023-10-18 00:26:13 -0700

    review this problem

--- main_problem/multi_pointer/interval/57_insert_interval.py
+++ main_problem/multi_pointer/interval/57_insert_interval.py
@@ -6,23 +6,31 @@ class Solution:
         if not intervals:
             return [newInterval]
         start, end = newInterval
+        if end < intervals[0][0]:
+            intervals.insert(0, newInterval)
+            return intervals
+        if start > intervals[-1][1]:
+            intervals.append(newInterval)
+            return intervals
         n = len(intervals)
         result = []
+        intervals[0][0] = min(start, intervals[0][0])
         for i in range(n):
-            if start > intervals[i][1] or end < intervals[i][0]:
-                result.append(intervals[i])
-            if end < intervals[i][0] and start <= newInterval[0]:
+            if end < intervals[i][0] and start != -1:
                 result.append([start, end])
-                start = end
+                start = -1
             elif intervals[i][0] <= start and end <= intervals[i][1]:
                 result.append(intervals[i])
-                start = end
+                start = -1
             elif intervals[i][0] <= start <= intervals[i][1]:
                 start = intervals[i][0]
             elif intervals[i][0] <= end <= intervals[i][1]:
                 end = intervals[i][1]
                 result.append([start, end])
-                start = end
-            else:
+                start = -1
+            if start > intervals[i][1] or end < intervals[i][0]:
                 result.append(intervals[i])
+        # result.sort(key=lambda x:x[0])
+        if end > intervals[n - 1][1]:
+            result.append([start, end])
         return result
