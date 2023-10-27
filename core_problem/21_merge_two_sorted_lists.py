be061cc1de81363e0b0940805ff6bc35b8e9b360
liuyunan <liuyunan.20@gmail.com>
2023-10-26 21:00:24 -0700

    review this problem

--- core_problem/21_merge_two_sorted_lists.py
+++ core_problem/21_merge_two_sorted_lists.py
@@ -7,35 +7,19 @@ class ListNode:
 
 class Solution:
     def mergeTwoLists(self, list1: ListNode, list2: ListNode) -> ListNode:
-        i = list1
-        j = list2
-        pre = cur = ListNode()
-        while i and j:
-            if i.val <= j.val:
-                cur.next = i
-                i = i.next
-
-            else:
-                cur.next = j
-                j = j.next
-            cur = cur.next
-        if i:
-            cur.next = i
-        else:
-            cur.next = j
-        return pre.next
-
-    def mergeTwoLists_2(self, list1: ListNode, list2: ListNode) -> ListNode:
         result = ListNode()
         node = result
         while list1 and list2:
-            if list1.val < list2.val:
-                node.next = ListNode(list1.val)
+            if list1.val <= list2.val:
+                node.next = list1
+                node = list1
                 list1 = list1.next
             else:
-                node.next = ListNode(list2.val)
+                node.next = list2
+                node = list2
                 list2 = list2.next
-            node = node.next
-
-        node.next = list1 if list1 else list2
+        if list1:
+            node.next = list1
+        else:
+            node.next = list2
         return result.next
