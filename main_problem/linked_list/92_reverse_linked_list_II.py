cf5c6df169b701b2a96a8d43776abeea867ba02c
liuyunan <liuyunan.20@gmail.com>
2023-10-28 22:21:45 -0700

    review this problem

--- main_problem/linked_list/92_reverse_linked_list_II.py
+++ main_problem/linked_list/92_reverse_linked_list_II.py
@@ -7,31 +7,25 @@ class ListNode:
 
 class Solution:
     def reverseBetween(self, head: Optional[ListNode], left: int, right: int) -> Optional[ListNode]:
-        dummy = ListNode(0, head)
-        pre = dummy
-        nodes = []
         i = 1
-        while i < left:
-            pre = pre.next
-            head = head.next
+        result = head
+        nodes = []
+        while head:
+            if i == left - 1:
+                hook_left = head
+            if left <= i <= right:
+                nodes.append(head)
             i += 1
-        left_hook = pre
-        left_helper = head
-
-        while i <= right:
-            nodes.append(head)
-            pre = pre.next
             head = head.next
-            i += 1
-        right_helper = pre
-        right_hook = head
-
-        dummy_hook = ListNode(0, right_helper)
+        hook_right = nodes[-1].next
+        start = nodes.pop()
+        current = start
         while nodes:
-            node = nodes.pop()
-            if nodes:
-                node.next = nodes[-1]
-            else:
-                node.next = right_hook
-        left_hook.next = dummy_hook.next
-        return dummy.next
+            current.next = nodes.pop()
+            current = current.next
+        current.next = hook_right
+        if left == 1:
+            return start
+        else:
+            hook_left.next = start
+            return result
